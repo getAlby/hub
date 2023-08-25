85d95ad8cc534ca6fffc6f89062b2fc14cd01094
kiwiidb <kwinten.debacker@hotmail.com>
2023-08-25 11:32:45 +0200

    implement interfaces

--- breez/breez.go
+++ breez/breez.go
@@ -41,14 +41,30 @@ func NewBreezService(mnemonic, apiKey, inviteCode, workDir string) (result *Bree
 }
 
 func (bs *BreezService) SendPaymentSync(ctx context.Context, senderPubkey string, payReq string) (preimage string, err error) {
-	return "", nil
+	resp, err := bs.svc.SendPayment(payReq, nil)
+	if err != nil {
+		return "", err
+	}
+	var lnDetails breez_sdk.PaymentDetailsLn
+	if resp.Details != nil {
+		lnDetails, _ = resp.Details.(breez_sdk.PaymentDetailsLn)
+	}
+	return lnDetails.Data.PaymentPreimage, nil
 
 }
 
 func (bs *BreezService) GetBalance(ctx context.Context, senderPubkey string) (balance int64, err error) {
-	return 0, nil
+	info, err := bs.svc.NodeInfo()
+	if err != nil {
+		return 0, err
+	}
+	return int64(info.ChannelsBalanceMsat) / 1000, nil
 }
 
 func (bs *BreezService) MakeInvoice(ctx context.Context, senderPubkey string, amount int64, description string, descriptionHash string, expiry int64) (invoice string, paymentHash string, err error) {
-	return "", "", nil
+	resp, err := bs.svc.ReceivePayment(uint64(amount), description)
+	if err != nil {
+		return "", "", err
+	}
+	return resp.Bolt11, resp.PaymentHash, nil
 }
