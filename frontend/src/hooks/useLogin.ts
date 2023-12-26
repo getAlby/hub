fa6599234f3350e0a443ec0ea23706900fdb322a
Roland Bewick <roland.bewick@gmail.com>
2023-12-26 12:12:41 +0700

    fix: do not redirect to login page from about page

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -3,14 +3,17 @@ import { useInfo } from "./useInfo";
 import React from "react";
 
 export function useLogin() {
-  // TODO:
   const { data: info, isLoading } = useInfo();
   const location = useLocation();
   const navigate = useNavigate();
 
   React.useEffect(() => {
     // TODO: Use the location to redirect back in /alby/auth?c=
-    if (!isLoading && !info?.user && location.pathname !== "/login") {
+    if (
+      !isLoading &&
+      !info?.user &&
+      ["/login", "/about"].indexOf(location.pathname) < 0
+    ) {
       navigate("/login");
     }
   }, [info?.user, isLoading, location.pathname, navigate]);
