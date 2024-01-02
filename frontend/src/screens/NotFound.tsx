1426f3c54dab21329b0eb53e45bfdf392fb256eb
im-adithya <imadithyavardhan@gmail.com>
2024-01-02 16:58:06 +0530

    chore: not found styling

--- frontend/src/screens/NotFound.tsx
+++ frontend/src/screens/NotFound.tsx
@@ -1,15 +1,19 @@
 function NotFound() {
   return (
-    <div className="bg-white rounded-md shadow px-4 lg:p-8 dark:bg-surface-02dp">
-      <h3 className="font-bold text-2xl font-headline mb-4 dark:text-white">
-        404 Page Not Found
-      </h3>
-      <p className="leading-relaxed dark:text-neutral-400 mb-4">
-        The page you are looking for does not exist.
-      </p>
-      <a href="/" className="text-purple-700 dark:text-purple-400">
-        Return Home
-      </a>
+    <div className="flex justify-center">
+      <div className="container max-w-screen-lg">
+        <div className="bg-white rounded-md shadow my-4 px-4 lg:p-8 dark:bg-surface-02dp">
+          <h3 className="font-bold text-2xl font-headline mb-4 dark:text-white">
+            404 Page Not Found
+          </h3>
+          <p className="leading-relaxed dark:text-neutral-400 mb-4">
+            The page you are looking for does not exist.
+          </p>
+          <a href="/" className="text-purple-700 dark:text-purple-400">
+            Return Home
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
