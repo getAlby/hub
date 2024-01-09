20e10f5ee6910eaffd9b35d83de7a760898106a7
René Aaron <rene@twentyuno.net>
2024-01-09 23:30:32 +0000

    fix: sql

--- migrations/202401092201_add_events_id_index.go
+++ migrations/202401092201_add_events_id_index.go
@@ -9,11 +9,7 @@ import (
 var _202401092201_add_events_id_index = &gormigrate.Migration{
 	ID: "202401092201_add_events_id_index",
 	Migrate: func(tx *gorm.DB) error {
-
-		var sql string
-		sql = "CREATE INDEX idx_nostr_events_app_id_and_id ON nostr_events(app_id, id)"
-
-		return tx.Exec(sql).Error
+		return tx.Exec("CREATE INDEX idx_nostr_events_app_id_and_id ON nostr_events(app_id, id)").Error
 	},
 	Rollback: func(tx *gorm.DB) error {
 		return nil
