cfaebd911ca883a324a6e4c142f8f13b518a98d8
Roland Bewick <roland.bewick@gmail.com>
2024-01-19 23:34:58 +0700

    chore: error handling

--- main.go
+++ main.go
@@ -231,6 +231,7 @@ func (svc *Service) launchLNBackend() error {
 		svc.Logger.Fatalf("Unsupported LNBackendType: %v", dbConfig.LNBackendType)
 	}
 	if err != nil {
+		svc.Logger.Errorf("Failed to launch LN backend: %v", err)
 		return err
 	}
 	svc.lnClient = lnClient
