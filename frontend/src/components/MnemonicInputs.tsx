2b420b5a6deca6ebe272a641d20bcc1d8585b744
im-adithya <imadithyavardhan@gmail.com>
2024-02-14 16:53:40 +0530

    chore: fix mnemonic inputs

--- frontend/src/components/MnemonicInputs.tsx
+++ frontend/src/components/MnemonicInputs.tsx
@@ -24,6 +24,10 @@ export default function MnemonicInputs({
     words.push("");
   }
 
+  while (words.length > 12) {
+    words.pop();
+  }
+
   return (
     <div className="border border-gray-200 dark:border-neutral-700 bg-white dark:bg-surface-02dp rounded-lg p-6 flex flex-col gap-8 items-center justify-center w-full mx-auto">
       <h3 className="text-lg font-semibold dark:text-white">
