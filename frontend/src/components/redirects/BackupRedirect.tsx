0c857bc915cd2bbd817a11eadda11b06952affa8
im-adithya <imadithyavardhan@gmail.com>
2024-03-27 16:15:23 +0530

    chore: add backup redirect

---
+++ frontend/src/components/redirects/BackupRedirect.tsx
@@ -0,0 +1,22 @@
+import { useInfo } from "src/hooks/useInfo";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import React from "react";
+import Loading from "src/components/Loading";
+
+export function BackupRedirect() {
+  const { data: info } = useInfo();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (info && !info.setupCompleted) {
+      navigate("/welcome");
+    }
+  }, [info, location, navigate]);
+
+  if (!info) {
+    return <Loading />;
+  }
+
+  return <Outlet />;
+}
