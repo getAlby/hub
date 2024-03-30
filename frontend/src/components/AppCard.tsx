9e01a3d207697243765da1e92980ce9163179370
René Aaron <rene@twentyuno.net>
2024-03-31 00:42:53 +0100

    fix: appcards

--- frontend/src/components/AppCard.tsx
+++ frontend/src/components/AppCard.tsx
@@ -35,7 +35,7 @@ export default function AppCard({ app }: Props) {
                     {app.name.charAt(0)}
                   </span>
                 </div>
-                <h2 className="flex-1 leading-5 font-semibold whitespace-nowrap text-ellipsis overflow-hidden ml-4">
+                <h2 className="flex-1 leading-5 font-semibold text-xl whitespace-nowrap text-ellipsis overflow-hidden ml-4">
                   {app.name}
                 </h2>
               </div>
@@ -45,11 +45,27 @@ export default function AppCard({ app }: Props) {
             {app.requestMethods?.includes(NIP_47_PAY_INVOICE_METHOD) ? (
               app.maxAmount > 0 ? (
                 <>
-                  <div className="mb-2">
-                    <p className="text-sm">You've spent:</p>
-                    <p className="text-xl font-medium">
-                      {new Intl.NumberFormat().format(app.budgetUsage)} sats
-                    </p>
+                  <div className="flex flex-row justify-between">
+                    <div className="mb-2">
+                      <p className="text-xs text-secondary-foreground font-medium">
+                        You've spent
+                      </p>
+                      <p className="text-xl font-medium">
+                        {new Intl.NumberFormat().format(app.budgetUsage)} sats
+                      </p>
+                    </div>
+                    <div className="text-right">
+                      {" "}
+                      <p className="text-xs text-secondary-foreground font-medium">
+                        Left in budget
+                      </p>
+                      <p className="text-xl font-medium text-muted-foreground">
+                        {new Intl.NumberFormat().format(
+                          app.maxAmount - app.budgetUsage
+                        )}{" "}
+                        sats
+                      </p>
+                    </div>
                   </div>
                   <Progress
                     className="h-4"
@@ -62,6 +78,29 @@ export default function AppCard({ app }: Props) {
             ) : (
               "Payments disabled."
             )}
+            <div className="grid gap-2 mt-5 text-muted-foreground text-sm">
+              <div className="flex flex-row justify-between">
+                <div>Budget</div>
+                <div>
+                  {app.maxAmount > 0 ? (
+                    <>
+                      {new Intl.NumberFormat().format(app.maxAmount)} sats /{" "}
+                      {app.budgetRenewal}
+                    </>
+                  ) : (
+                    "Not set"
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-row justify-between">
+                <div>Expires on</div>
+                <div>{app.expiresAt ?? "Never"}</div>
+              </div>
+              <div className="flex flex-row justify-between">
+                <div>Last used</div>
+                <div>{app.lastEventAt ?? "Never"}</div>
+              </div>
+            </div>
           </CardContent>
         </Card>
       </Link>
