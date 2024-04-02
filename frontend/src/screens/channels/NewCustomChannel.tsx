bb8325ba6dc6bf1c5d76a5adbcc1d398fd64aa83
pavanjoshi914 <pavanj914@gmail.com>
2024-04-02 18:18:59 +0530

    migrate fully  customChannel component

--- frontend/src/screens/channels/NewCustomChannel.tsx
+++ frontend/src/screens/channels/NewCustomChannel.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
-import { Button } from "src/components/ui/button";
 import { Card, CardContent } from "src/components/ui/card";
+import { Checkbox } from "src/components/ui/checkbox";
+import { Input } from "src/components/ui/input";
+import { Label } from "src/components/ui/label";
+import { LoadingButton } from "src/components/ui/loading-button";
 import { useCSRF } from "src/hooks/useCSRF";
 import {
   ConnectPeerRequest,
@@ -171,13 +174,13 @@ export default function NewCustomChannel() {
 
           <div className="flex flex-wrap -mx-3 mt-6">
             <div className="w-full px-3 mb-6 md:mb-0">
-              <label
+              <Label
                 className="block uppercase tracking-wide text-primary text-xs font-bold mb-2"
                 htmlFor="grid-first-name"
               >
                 Peer
-              </label>
-              <input
+              </Label>
+              <Input
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 type="text"
                 value={pubkey}
@@ -189,13 +192,13 @@ export default function NewCustomChannel() {
             </div>
             {!nodeDetails && pubkey && (
               <div className="w-full px-3 mb-6 md:mb-0">
-                <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                <Label
+                  className="block uppercase tracking-wide text-primary text-xs font-bold mb-2"
                   htmlFor="grid-first-name"
                 >
                   Host:Port
-                </label>
-                <input
+                </Label>
+                <Input
                   className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                   type="text"
                   value={host}
@@ -210,13 +213,13 @@ export default function NewCustomChannel() {
 
           <div className="flex flex-wrap -mx-3 mt-6">
             <div className="w-full px-3 mb-6 md:mb-0">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              <Label
+                className="block uppercase tracking-wide text-primary text-xs font-bold mb-2"
                 htmlFor="grid-first-name"
               >
                 Amount (sats)
-              </label>
-              <input
+              </Label>
+              <Input
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 type="text"
                 value={localAmount}
@@ -227,44 +230,25 @@ export default function NewCustomChannel() {
             </div>
           </div>
 
-          <div className="w-full my-6">
-            <label>
-              <input
-                type="checkbox"
-                defaultChecked={isPublic}
-                onChange={(e) => setPublic(e.target.checked)}
-                className="mr-2"
-              />
-              Public Channel
-            </label>
+          <div className="flex w-full my-6 items-center">
+            <Checkbox
+              id="public-channel"
+              defaultChecked={isPublic}
+              onCheckedChange={() => setPublic(!isPublic)}
+              className="mr-2"
+            />
+            <Label htmlFor="public-channel">Public Channel</Label>
           </div>
 
           <div className="mt-2">
-            <Button
+            <LoadingButton
+              size={"sm"}
               disabled={!pubkey || !localAmount || loading}
               onClick={openChannel}
+              loading={loading}
             >
-              {loading && (
-                <svg
-                  aria-hidden="true"
-                  role="status"
-                  className="inline mr-2 w-4 h-4 text-muted-foreground animate-spin"
-                  viewBox="0 0 100 101"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="currentColor"
-                  />
-                  <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="#1C64F2"
-                  />
-                </svg>
-              )}
               {loading ? "Loading..." : "Open Channel"}
-            </Button>
+            </LoadingButton>
           </div>
         </CardContent>
       </Card>
