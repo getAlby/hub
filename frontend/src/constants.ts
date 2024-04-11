fc64e494fe80c0c4cad1c6cf4fd0debff7dcfae3
Roland Bewick <roland.bewick@gmail.com>
2024-04-11 12:52:47 +0700

    fix: incorrect min alby balance

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -7,6 +7,6 @@ export const MIN_0CONF_BALANCE = 30000; // 30,000 for Alby. 20000 works for Olym
 export const ALBY_SERVICE_FEE = 8 / 1000;
 export const ALBY_FEE_RESERVE = 0.01; /* TODO: remove when no fee reserve is needed */
 export const MIN_ALBY_BALANCE =
-  MIN_0CONF_BALANCE + 31000; /* TODO: remove when no fee reserve is needed */
+  MIN_0CONF_BALANCE + 1000; /* TODO: remove when no fee reserve is needed */
 
 export const ONCHAIN_DUST_SATS = 1000;
