da2a6e8c351700e86c3c437329bc52b01b2f688e
René Aaron <rene@twentyuno.net>
2024-04-13 12:59:43 +0200

    fix: remove placeholder settings

--- frontend/src/components/layouts/SettingsLayout.tsx
+++ frontend/src/components/layouts/SettingsLayout.tsx
@@ -1,3 +1,4 @@
+import { ExternalLink } from "lucide-react";
 import { NavLink, Outlet } from "react-router-dom";
 import AppHeader from "src/components/AppHeader";
 import { buttonVariants } from "src/components/ui/button";
@@ -11,14 +12,19 @@ export default function SettingsLayout() {
         description="Manage your account settings and set e-mail preferences."
       />
       <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-        <nav className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1">
-          <MenuItem to="/settings">General</MenuItem>
-          <MenuItem to="/settings/backup">Backup</MenuItem>
-          <MenuItem to="/settings/change-unlock-password">
-            Unlock Password
-          </MenuItem>
-          <MenuItem to="/debug-tools">Debug Tools</MenuItem>
-        </nav>
+        <aside className="-mx-4 lg:w-1/5">
+          <nav className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1">
+            <MenuItem to="/settings">General</MenuItem>
+            <MenuItem to="/settings/backup">Backup</MenuItem>
+            <MenuItem to="/settings/change-unlock-password">
+              Unlock Password
+            </MenuItem>
+            <MenuItem to="/debug-tools">
+              Debug Tools
+              <ExternalLink className="w-4 h-4 ml-2" />
+            </MenuItem>
+          </nav>
+        </aside>
         <div className="flex-1 lg:max-w-2xl">
           <div className="grid gap-6">
             <Outlet />
--- frontend/src/screens/settings/Settings.tsx
+++ frontend/src/screens/settings/Settings.tsx
@@ -1,66 +1,12 @@
 import SettingsHeader from "src/components/SettingsHeader";
-import { Button } from "src/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "src/components/ui/card";
-import { Checkbox } from "src/components/ui/checkbox";
-import { Input } from "src/components/ui/input";
 
 function Settings() {
   return (
     <>
       <SettingsHeader
         title="General"
-        description="Customize the appearance of the app. Automatically switch between
-            day and night themes."
+        description="Adjust general settings of your Alby Hub."
       />
-      <Card>
-        <CardHeader>
-          <CardTitle>Store Name</CardTitle>
-          <CardDescription>
-            Used to identify your store in the marketplace.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <Input placeholder="Store Name" />
-          </form>
-        </CardContent>
-        <CardFooter className="border-t px-6 py-4">
-          <Button>Save</Button>
-        </CardFooter>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>Plugins Directory</CardTitle>
-          <CardDescription>
-            The directory within your project, in which your plugins are
-            located.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form className="flex flex-col gap-4">
-            <Input placeholder="Project Name" defaultValue="/content/plugins" />
-            <div className="flex items-center space-x-2">
-              <Checkbox id="include" defaultChecked />
-              <label
-                htmlFor="include"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                Allow administrators to change the directory.
-              </label>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="border-t px-6 py-4">
-          <Button>Save</Button>
-        </CardFooter>
-      </Card>
     </>
   );
 }
