f47592bdde765768764f795a4d061ee0bf985f94
René Aaron <rene@twentyuno.net>
2024-04-13 17:44:28 +0200

    fix: switch icon bg

--- frontend/src/screens/wallet/index.tsx
+++ frontend/src/screens/wallet/index.tsx
@@ -239,7 +239,7 @@ function Wallet() {
           <Card>
             <CardHeader>
               <div className="flex flex-row items-center">
-                <img src={AlbyHead} className="w-12 h-12 rounded-xl p-1 border bg-[#FFDF6F]" />
+                <img src={AlbyHead} className="w-12 h-12 rounded-xl p-1 border" />
                 <div>
                   <CardTitle>
                     <h2 className="flex-1 leading-5 font-semibold text-xl whitespace-nowrap text-ellipsis overflow-hidden ml-4">
@@ -265,7 +265,7 @@ function Wallet() {
           <Card>
             <CardHeader>
               <div className="flex flex-row items-center">
-                <img src={AlbyHead} className="w-12 h-12 rounded-xl p-1 border" />
+                <img src={AlbyHead} className="w-12 h-12 rounded-xl p-1 border bg-[#FFDF6F]" />
                 <div>
                   <CardTitle>
                     <h2 className="flex-1 leading-5 font-semibold text-xl whitespace-nowrap text-ellipsis overflow-hidden ml-4">
