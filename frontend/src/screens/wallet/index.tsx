c1aaf62799ef4571288d297f33e7e07f7cf0935d
René Aaron <rene@twentyuno.net>
2024-05-28 13:51:42 +0200

    fix: remove wallet status

--- frontend/src/screens/wallet/index.tsx
+++ frontend/src/screens/wallet/index.tsx
@@ -1,8 +1,4 @@
 import {
-  ArrowDownToDot,
-  ArrowUpFromDot,
-  CircleDot,
-  CopyIcon,
   ExternalLink
 } from "lucide-react";
 import { Link } from "react-router-dom";
@@ -18,19 +14,9 @@ import {
   CardHeader,
   CardTitle,
 } from "src/components/ui/card";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "src/components/ui/dropdown-menu";
 import { useBalances } from "src/hooks/useBalances";
 import { useInfo } from "src/hooks/useInfo";
 import { useNodeConnectionInfo } from "src/hooks/useNodeConnectionInfo";
-import { copyToClipboard } from "src/lib/clipboard";
 
 function Wallet() {
   const { data: info } = useInfo();
@@ -50,79 +36,6 @@ function Wallet() {
       <AppHeader
         title="Wallet"
         description="Send and receive transactions"
-        contentRight={
-          isWalletUsable && (
-            <>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="outline" size="default">
-                    <CircleDot className="mr-2 h-4 w-4 text-primary" />
-                    Online
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-72" align="end">
-                  <DropdownMenuItem>
-                    <div className="flex flex-row gap-10 items-center w-full">
-                      <div className="whitespace-nowrap flex flex-row items-center gap-2">
-                        Node
-                      </div>
-                      <div className="overflow-hidden text-ellipsis">
-                        {/* TODO: replace with skeleton loader */}
-                        {nodeConnectionInfo?.pubkey || "Loading..."}
-                      </div>
-                      {nodeConnectionInfo && (
-                        <CopyIcon
-                          className="shrink-0 w-4 h-4"
-                          onClick={() => {
-                            copyToClipboard(nodeConnectionInfo.pubkey);
-                          }}
-                        />
-                      )}
-                    </div>
-                  </DropdownMenuItem>
-                  <>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuGroup>
-                      <DropdownMenuLabel>Liquidity</DropdownMenuLabel>
-                      <DropdownMenuItem>
-                        <div className="flex flex-row gap-3 items-center justify-between w-full">
-                          <div className="grid grid-flow-col gap-2 items-center">
-                            <ArrowDownToDot className="w-4 h-4 " />
-                            Receiving Capacity
-                          </div>
-                          <div className="text-muted-foreground text-right">
-                            {new Intl.NumberFormat().format(
-                              Math.floor(
-                                balances.lightning.totalReceivable / 1000
-                              )
-                            )}{" "}
-                            sats
-                          </div>
-                        </div>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem>
-                        <div className="flex flex-row gap-3 items-center justify-between w-full">
-                          <div className="grid grid-flow-col gap-2 items-center">
-                            <ArrowUpFromDot className="w-4 h-4 " />
-                            Spending Balance
-                          </div>
-                          <div className="text-muted-foreground text-right">
-                            {new Intl.NumberFormat().format(
-                              Math.floor(
-                                balances.lightning.totalSpendable / 1000
-                              )
-                            )}{" "}
-                            sats
-                          </div>
-                        </div>
-                      </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                  </>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </>
-          )
-        }
       />
 
       <div className="flex flex-col lg:flex-row justify-between items-start lg:items-end gap-5">
