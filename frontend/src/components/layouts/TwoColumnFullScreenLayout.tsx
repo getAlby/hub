a2b07a539a063dfc97b8ecbcf23208165678fc38
René Aaron <100827540+reneaaron@users.noreply.github.com>
2024-06-05 20:46:28 +0200

    feat: quotes (#380)

--- frontend/src/components/layouts/TwoColumnFullScreenLayout.tsx
+++ frontend/src/components/layouts/TwoColumnFullScreenLayout.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 const quotes = [
   {
@@ -9,34 +9,57 @@ const quotes = [
         largest economy. It is the first transnational economy, and it needs a
         transnational currency.`,
     author: "Andreas M. Antonopoulos",
-    imageUrl: "/images/quotes/antonopoulos.webp",
+    imageUrl: "/images/quotes/antonopoulos.svg",
   },
   {
     content: `It might make sense just to get some in case it catches on. If enough people think the same way, that becomes a self fulfilling prophecy. Once it gets bootstrapped, there are so many applications if you could effortlessly pay a few cents to a website as easily as dropping coins in a vending machine.`,
     author: "Satoshi Nakamoto",
-    imageUrl: "/images/quotes/nakamoto.webp",
+    imageUrl: "/images/quotes/nakamoto.svg",
   },
   {
     content: `Since we’re all rich with bitcoins, or we will be once they’re worth a million dollars like everyone expects, we ought to put some of this unearned wealth to good use.`,
     author: "Hal Finney",
-    imageUrl: "/images/quotes/finney.jpg",
+    imageUrl: "/images/quotes/finney.svg",
   },
   {
     content: `I don't believe we shall ever have a good money again before we take the thing out of the hands of government, that is, we can't take it violently out of the hands of government, all we can do is by some sly roundabout way introduce something that they can't stop.`,
     author: "Friedrich August von Hayek",
-    imageUrl: "/images/quotes/hayek.webp",
+    imageUrl: "/images/quotes/hayek.svg",
+  },
+  {
+    content: `Bitcoin is what they fear it is.`,
+    author: "Cody Wilson",
+    imageUrl: "/images/quotes/wilson.svg",
+  },
+  {
+    content: `If in fact you can't crack that at all, government can't get in then —everybody's walking around with a Swiss bank account in their pocket.`,
+    author: "Barack Obama",
+    imageUrl: "/images/quotes/obama.svg",
+  },
+  {
+    content: `Bitcoin is the new wonder of the world, more work and human ingenuity, than went into the great pyramids of Egypt. The biggest computation ever done, a digital monument, a verifiable artefact of digital gold - the foundation of a new digital age.`,
+    author: "Adam Back",
+    imageUrl: "/images/quotes/back.svg",
   },
 ];
 
 export default function TwoColumnFullScreenLayout() {
-  const quote = React.useMemo(
-    () => quotes[Math.floor(Math.random() * quotes.length)],
-    []
+  const { pathname } = useLocation();
+  const [quote, setQuote] = useState(
+    quotes[Math.floor(Math.random() * quotes.length)]
   );
+
+  // Change quote on route changes
+  useEffect(() => {
+    setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+  }, [pathname]);
 
   return (
     <div className="w-full lg:grid lg:h-screen lg:grid-cols-2 items-stretch text-background">
-      <div className="hidden lg:flex flex-col bg-foreground  justify-end p-10 gap-2">
+      <div
+        className="hidden lg:flex flex-col bg-foreground justify-end p-10 gap-2 bg-cover bg-no-repeat bg-bottom"
+        style={{ backgroundImage: `url(${quote.imageUrl})` }}
+      >
         <div className="flex-1 w-full h-full flex flex-col">
           <div className="flex flex-row justify-between">
             <h1 className="text-lg font-medium">Alby Hub</h1>
@@ -44,10 +67,6 @@ export default function TwoColumnFullScreenLayout() {
           </div>
         </div>
         <div className="flex flex-row gap-5">
-          <div
-            className="w-40 h-40 shrink-0 bg-[] grayscale bg-cover bg-no-repeat bg-top rounded"
-            style={{ backgroundImage: `url(${quote.imageUrl})` }}
-          ></div>
           <div className="flex flex-col justify-center">
             <p className="text-muted-foreground text-lg mb-2">
               {quote.content}
