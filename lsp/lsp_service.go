a7ddb6d31d0227dc460b5c5cd78f3422e5bcef3b
Roland <33993199+rolznz@users.noreply.github.com>
2024-06-15 13:42:12 +0700

    fix: respect LSPS1 channel max expiry blocks (#449)

--- lsp/lsp_service.go
+++ lsp/lsp_service.go
@@ -23,10 +23,11 @@ type lspService struct {
 	logger *logrus.Logger
 }
 
-type lspConnectionInfo struct {
-	Pubkey  string
-	Address string
-	Port    uint16
+type lspInfo struct {
+	Pubkey                 string
+	Address                string
+	Port                   uint16
+	MaxChannelExpiryBlocks uint64
 }
 
 func NewLSPService(svc service.Service, logger *logrus.Logger) *lspService {
@@ -69,7 +70,7 @@ func (ls *lspService) NewInstantChannelInvoice(ctx context.Context, request *New
 
 	ls.logger.Infoln("Requesting LSP info")
 
-	var lspInfo *lspConnectionInfo
+	var lspInfo *lspInfo
 	var err error
 	switch selectedLsp.LspType {
 	case LSP_TYPE_FLOW_2_0:
@@ -120,7 +121,7 @@ func (ls *lspService) NewInstantChannelInvoice(ctx context.Context, request *New
 	case LSP_TYPE_PMLSP:
 		invoice, fee, err = ls.requestPMLSPInvoice(&selectedLsp, request.Amount, nodeInfo.Pubkey)
 	case LSP_TYPE_LSPS1:
-		invoice, fee, err = ls.requestLSPS1Invoice(ctx, &selectedLsp, request.Amount, nodeInfo.Pubkey, request.Public)
+		invoice, fee, err = ls.requestLSPS1Invoice(ctx, &selectedLsp, request.Amount, nodeInfo.Pubkey, request.Public, lspInfo.MaxChannelExpiryBlocks)
 
 	default:
 		return nil, fmt.Errorf("unsupported LSP type: %v", selectedLsp.LspType)
@@ -142,13 +143,16 @@ func (ls *lspService) NewInstantChannelInvoice(ctx context.Context, request *New
 	return newChannelResponse, nil
 }
 
-func (ls *lspService) getLSPS1LSPInfo(url string) (*lspConnectionInfo, error) {
-	type LSPS1LSPInfo struct {
-		// TODO: implement options
-		Options interface{} `json:"options"`
-		URIs    []string    `json:"uris"`
+func (ls *lspService) getLSPS1LSPInfo(url string) (*lspInfo, error) {
+
+	type lsps1LSPInfoOptions struct {
+		MaxChannelExpiryBlocks uint64 `json:"max_channel_expiry_blocks"`
 	}
-	var lsps1LspInfo LSPS1LSPInfo
+	type lsps1LSPInfo struct {
+		Options lsps1LSPInfoOptions `json:"options"`
+		URIs    []string            `json:"uris"`
+	}
+	var lsps1LspInfo lsps1LSPInfo
 	client := http.Client{
 		Timeout: time.Second * 10,
 	}
@@ -204,13 +208,14 @@ func (ls *lspService) getLSPS1LSPInfo(url string) (*lspConnectionInfo, error) {
 		return nil, err
 	}
 
-	return &lspConnectionInfo{
-		Pubkey:  parts[1],
-		Address: parts[2],
-		Port:    uint16(port),
+	return &lspInfo{
+		Pubkey:                 parts[1],
+		Address:                parts[2],
+		Port:                   uint16(port),
+		MaxChannelExpiryBlocks: lsps1LspInfo.Options.MaxChannelExpiryBlocks,
 	}, nil
 }
-func (ls *lspService) getFlowLSPInfo(url string) (*lspConnectionInfo, error) {
+func (ls *lspService) getFlowLSPInfo(url string) (*lspInfo, error) {
 	type FlowLSPConnectionMethod struct {
 		Address string `json:"address"`
 		Port    uint16 `json:"port"`
@@ -271,7 +276,7 @@ func (ls *lspService) getFlowLSPInfo(url string) (*lspConnectionInfo, error) {
 		return nil, errors.New("unexpected LSP connection method")
 	}
 
-	return &lspConnectionInfo{
+	return &lspInfo{
 		Pubkey:  flowLspInfo.Pubkey,
 		Address: flowLspInfo.ConnectionMethods[ipIndex].Address,
 		Port:    flowLspInfo.ConnectionMethods[ipIndex].Port,
@@ -520,7 +525,7 @@ func (ls *lspService) requestPMLSPInvoice(selectedLsp *LSP, amount uint64, pubke
 	return invoice, fee, nil
 }
 
-func (ls *lspService) requestLSPS1Invoice(ctx context.Context, selectedLsp *LSP, amount uint64, pubkey string, public bool) (invoice string, fee uint64, err error) {
+func (ls *lspService) requestLSPS1Invoice(ctx context.Context, selectedLsp *LSP, amount uint64, pubkey string, public bool, channelExpiryBlocks uint64) (invoice string, fee uint64, err error) {
 	client := http.Client{
 		Timeout: time.Second * 10,
 	}
@@ -557,7 +562,7 @@ func (ls *lspService) requestLSPS1Invoice(ctx context.Context, selectedLsp *LSP,
 		ClientBalanceSat:             "0",
 		RequiredChannelConfirmations: requiredChannelConfirmations,
 		FundingConfirmsWithinBlocks:  6,
-		ChannelExpiryBlocks:          13000, // TODO: this should be customizable
+		ChannelExpiryBlocks:          channelExpiryBlocks,
 		Token:                        "",
 		RefundOnchainAddress:         refundAddress,
 		AnnounceChannel:              public,
