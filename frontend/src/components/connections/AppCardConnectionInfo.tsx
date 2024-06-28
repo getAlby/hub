3206b623e88b34aeef560f66be8061209bcb1716
René Aaron <100827540+reneaaron@users.noreply.github.com>
2024-06-28 16:21:24 +0200

    fix: add info for apps without budgets (#524)
    Co-authored-by: Roland Bewick <roland.bewick@gmail.com>

--- frontend/src/components/connections/AppCardConnectionInfo.tsx
+++ frontend/src/components/connections/AppCardConnectionInfo.tsx
@@ -1,4 +1,6 @@
 import dayjs from "dayjs";
+import { PlusCircle } from "lucide-react";
+import { Button } from "src/components/ui/button";
 import { Progress } from "src/components/ui/progress";
 import { formatAmount } from "src/lib/utils";
 import { App } from "src/types";
@@ -12,7 +14,7 @@ export function AppCardConnectionInfo({
 }: AppCardConnectionInfoProps) {
   return (
     <>
-      {connection.maxAmount > 0 && (
+      {connection.maxAmount > 0 ? (
         <>
           <div className="flex flex-row justify-between">
             <div className="mb-2">
@@ -52,6 +54,41 @@ export function AppCardConnectionInfo({
               </div>
             </div>
           )}
+        </>
+      ) : connection.requestMethods.indexOf("pay_invoice") > -1 ? (
+        <>
+          <div className="flex flex-row justify-between">
+            <div className="mb-2">
+              <p className="text-xs text-secondary-foreground font-medium">
+                You've spent
+              </p>
+              <p className="text-xl font-medium">
+                {new Intl.NumberFormat().format(connection.budgetUsage)} sats
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row justify-end items-center">
+            <Button variant="outline">
+              <PlusCircle className="w-4 h-4 mr-2" />
+              Set Budget
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex flex-row justify-between h-full">
+            <div className="mb-2">
+              <p className="text-xs text-secondary-foreground font-medium">
+                Read only
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-1 flex-row justify-end items-center">
+            <Button variant="outline">
+              <PlusCircle className="w-4 h-4 mr-2" />
+              Enable Payments
+            </Button>
+          </div>
         </>
       )}
     </>
