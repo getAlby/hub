1c0b7e915397498c158c675616b10d02939b58d8
Roland Bewick <roland.bewick@gmail.com>
2024-07-18 12:53:00 +0700

    fix: get balance tests

--- nip47/controllers/get_balance_controller_test.go
+++ nip47/controllers/get_balance_controller_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/nbd-wtf/go-nostr"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/getAlby/hub/constants"
 	"github.com/getAlby/hub/db"
 	"github.com/getAlby/hub/nip47/models"
 	"github.com/getAlby/hub/nip47/permissions"
@@ -49,11 +50,11 @@ func TestHandleGetBalanceEvent(t *testing.T) {
 	NewNip47Controller(svc.LNClient, svc.DB, svc.EventPublisher, permissionsSvc, transactionsSvc).
 		HandleGetBalanceEvent(ctx, nip47Request, dbRequestEvent.ID, app, publishResponse)
 
-	assert.Equal(t, int64(21000), publishedResponse.Result.(*getBalanceResponse).Balance)
+	assert.Equal(t, uint64(21000), publishedResponse.Result.(*getBalanceResponse).Balance)
 	assert.Nil(t, publishedResponse.Error)
 }
 
-func TestHandleGetBalanceEvent_IsolatedApp(t *testing.T) {
+func TestHandleGetBalanceEvent_IsolatedApp_NoTransactions(t *testing.T) {
 	ctx := context.TODO()
 	defer tests.RemoveTestService()
 	svc, err := tests.CreateTestService()
@@ -83,7 +84,53 @@ func TestHandleGetBalanceEvent_IsolatedApp(t *testing.T) {
 	NewNip47Controller(svc.LNClient, svc.DB, svc.EventPublisher, permissionsSvc, transactionsSvc).
 		HandleGetBalanceEvent(ctx, nip47Request, dbRequestEvent.ID, app, publishResponse)
 
-	// FIXME: implement
-	assert.Equal(t, int64(1234567), publishedResponse.Result.(*getBalanceResponse).Balance)
+	assert.Equal(t, uint64(0), publishedResponse.Result.(*getBalanceResponse).Balance)
+	assert.Nil(t, publishedResponse.Error)
+}
+func TestHandleGetBalanceEvent_IsolatedApp_Transactions(t *testing.T) {
+	ctx := context.TODO()
+	defer tests.RemoveTestService()
+	svc, err := tests.CreateTestService()
+	assert.NoError(t, err)
+
+	nip47Request := &models.Request{}
+	err = json.Unmarshal([]byte(nip47GetBalanceJson), nip47Request)
+	assert.NoError(t, err)
+
+	app, _, err := tests.CreateApp(svc)
+	assert.NoError(t, err)
+	app.Isolated = true
+	svc.DB.Save(&app)
+
+	svc.DB.Create(&db.Transaction{
+		AppId:      &app.ID,
+		State:      constants.TRANSACTION_STATE_SETTLED,
+		Type:       constants.TRANSACTION_TYPE_INCOMING,
+		AmountMsat: 1000,
+	})
+	// create an unrelated transaction, should not count
+	svc.DB.Create(&db.Transaction{
+		AppId:      nil,
+		State:      constants.TRANSACTION_STATE_SETTLED,
+		Type:       constants.TRANSACTION_TYPE_INCOMING,
+		AmountMsat: 1000,
+	})
+
+	dbRequestEvent := &db.RequestEvent{}
+	err = svc.DB.Create(&dbRequestEvent).Error
+	assert.NoError(t, err)
+
+	var publishedResponse *models.Response
+
+	publishResponse := func(response *models.Response, tags nostr.Tags) {
+		publishedResponse = response
+	}
+
+	permissionsSvc := permissions.NewPermissionsService(svc.DB, svc.EventPublisher)
+	transactionsSvc := transactions.NewTransactionsService(svc.DB)
+	NewNip47Controller(svc.LNClient, svc.DB, svc.EventPublisher, permissionsSvc, transactionsSvc).
+		HandleGetBalanceEvent(ctx, nip47Request, dbRequestEvent.ID, app, publishResponse)
+
+	assert.Equal(t, uint64(1000), publishedResponse.Result.(*getBalanceResponse).Balance)
 	assert.Nil(t, publishedResponse.Error)
 }
