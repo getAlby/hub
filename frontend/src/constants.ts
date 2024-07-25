4f30883a7ffdc72565f58fd1bb0ddc74d54395a9
Roland Bewick <roland.bewick@gmail.com>
2024-07-25 15:26:20 +0700

    chore: remove unused constant

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -3,7 +3,5 @@ export const localStorageKeys = {
   channelOrder: "channelOrder",
 };
 
-export const ALBY_MIN_BALANCE = 20000;
-
 export const ONCHAIN_DUST_SATS = 1000;
 export const ALBY_HIDE_HOSTED_BALANCE_BELOW = 100;
