0949900ed7a3ce59493d7c3f0668c79b4e153ae9
René Aaron <rene@twentyuno.net>
2024-07-25 14:57:50 +0200

    feat: add minimum threshold, add illustration

--- frontend/src/screens/channels/first/FirstChannel.tsx
+++ frontend/src/screens/channels/first/FirstChannel.tsx
@@ -11,11 +11,16 @@ import { Label } from "src/components/ui/label";
 import { LoadingButton } from "src/components/ui/loading-button";
 import { Separator } from "src/components/ui/separator";
 import { useToast } from "src/components/ui/use-toast";
+import { ALBY_MIN_HOSTED_BALANCE_FOR_FIRST_CHANNEL } from "src/constants";
+import { useAlbyBalance } from "src/hooks/useAlbyBalance";
 import { useChannels } from "src/hooks/useChannels";
 import { useCSRF } from "src/hooks/useCSRF";
 import { useInfo } from "src/hooks/useInfo";
 import { AutoChannelRequest, AutoChannelResponse } from "src/types";
 import { request } from "src/utils/request";
+
+import lightningNetworkDark from "/images/illustrations/lightning-network-dark.svg";
+import lightningNetworkLight from "/images/illustrations/lightning-network-light.svg";
 
 export function FirstChannel() {
   const { data: info } = useInfo();
@@ -28,6 +33,7 @@ export function FirstChannel() {
   const { toast } = useToast();
   const [invoice, setInvoice] = React.useState<string>();
   const [channelSize, setChannelSize] = React.useState<number>();
+  const { data: albyBalance } = useAlbyBalance();
 
   React.useEffect(() => {
     if (channels?.length) {
@@ -75,15 +81,18 @@ export function FirstChannel() {
     }
   }
 
+  const canPayForFirstChannel =
+    albyBalance &&
+    albyBalance.sats >= ALBY_MIN_HOSTED_BALANCE_FOR_FIRST_CHANNEL;
+
   return (
     <>
       <AppHeader
         title="Open Your First Channel"
         description="Open a channel to another lightning network node to join the lightning network"
       />
-
       {invoice && channelSize && (
-        <div className="flex flex-col gap-4 items-center justify-center">
+        <div className="flex flex-col gap-4 items-center justify-center max-w-md">
           <p className="text-muted-foreground">
             Please pay the lightning invoice below which will cover the costs of
             opening your first channel. You will receive a channel with{" "}
@@ -98,7 +107,7 @@ export function FirstChannel() {
           </p>
           <Link to="/channels/outgoing" className="w-full">
             <Button className="w-full" variant="secondary">
-              Open Channel with Onchain Bitcoin
+              Open Channel with On-Chain Bitcoin
             </Button>
           </Link>
           <ExternalLink to="https://www.getalby.com/topup" className="w-full">
@@ -110,44 +119,78 @@ export function FirstChannel() {
       )}
       {!invoice && (
         <>
-          {showAdvanced && (
-            <>
-              <div className="mt-2 flex items-top space-x-2">
-                <Checkbox
-                  id="public-channel"
-                  onCheckedChange={() => setPublic(!isPublic)}
-                  className="mr-2"
-                />
-                <div className="grid gap-1.5 leading-none">
-                  <Label
-                    htmlFor="public-channel"
-                    className="flex items-center gap-2"
-                  >
-                    Public Channel
-                  </Label>
-                  <p className="text-xs text-muted-foreground">
-                    Only enable if you want to receive keysend payments. (e.g.
-                    podcasting)
-                  </p>
+          <div className="flex flex-col gap-4 max-w-md text-muted-foreground">
+            <img
+              src={lightningNetworkDark}
+              className="w-full hidden dark:block"
+            />
+            <img src={lightningNetworkLight} className="w-full dark:hidden" />
+            {canPayForFirstChannel ? (
+              <>
+                <p>
+                  You currently have{" "}
+                  <span className="font-medium">
+                    {new Intl.NumberFormat().format(albyBalance?.sats)} sats
+                  </span>{" "}
+                  on your Alby hosted balance.
+                </p>
+                <p>
+                  Those funds will be used to open your first lightning channel
+                  and then migrated to your Alby Hub's spending balance.
+                </p>
+              </>
+            ) : (
+              <>
+                <p>
+                  You're now going to open your first lightning channel and can
+                  begin use your Alby Hub in the booming bitcoin economy!
+                </p>
+                <p>
+                  After paying a lightning invoice to cover on-chain fees,
+                  you'll immediately be able to receive and send bitcoin with
+                  your Hub.
+                </p>
+              </>
+            )}
+            {showAdvanced && (
+              <>
+                <div className="mt-2 flex items-top space-x-2">
+                  <Checkbox
+                    id="public-channel"
+                    onCheckedChange={() => setPublic(!isPublic)}
+                    className="mr-2"
+                  />
+                  <div className="grid gap-1.5 leading-none">
+                    <Label
+                      htmlFor="public-channel"
+                      className="flex items-center gap-2"
+                    >
+                      Public Channel
+                    </Label>
+                    <p className="text-xs text-muted-foreground">
+                      Only enable if you want to receive keysend payments. (e.g.
+                      podcasting)
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </>
-          )}
-
-          <div className="flex flex-col gap-2 max-w-sm">
+              </>
+            )}
             {!showAdvanced && (
-              <Button
-                type="button"
-                variant="link"
-                className="text-muted-foreground text-xs"
-                onClick={() => setShowAdvanced((current) => !current)}
-              >
-                <ChevronDown className="w-4 h-4 mr-2" />
-                Advanced Options
-              </Button>
+              <div>
+                <Button
+                  type="button"
+                  variant="link"
+                  className="text-muted-foreground text-xs"
+                  onClick={() => setShowAdvanced((current) => !current)}
+                >
+                  <ChevronDown className="w-4 h-4 mr-2" />
+                  Advanced Options
+                </Button>
+              </div>
             )}
             <LoadingButton loading={isLoading} onClick={openChannel}>
               Open Channel
+              {albyBalance && albyBalance?.sats > 0 && <> and Migrate Funds</>}
             </LoadingButton>
           </div>
         </>
