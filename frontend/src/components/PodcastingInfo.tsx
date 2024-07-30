10afe14977a9ad32268128dbc585f3c84b7aaf1e
im-adithya <imadithyavardhan@gmail.com>
2024-07-30 20:45:08 +0530

    chore: remove ? in podcasting info component

--- frontend/src/components/PodcastingInfo.tsx
+++ frontend/src/components/PodcastingInfo.tsx
@@ -3,37 +3,37 @@ import { Boostagram } from "src/types";
 function PodcastingInfo({ boost }: { boost: Boostagram }) {
   return (
     <>
-      {boost?.message && (
+      {boost.message && (
         <div className="mt-6">
           <p>Message</p>
           <p className="text-muted-foreground break-all">{boost.message}</p>
         </div>
       )}
-      {boost?.podcast && (
+      {boost.podcast && (
         <div className="mt-6">
           <p>Podcast</p>
           <p className="text-muted-foreground break-all">{boost.podcast}</p>
         </div>
       )}
-      {boost?.episode && (
+      {boost.episode && (
         <div className="mt-6">
           <p>Episode</p>
           <p className="text-muted-foreground break-all">{boost.episode}</p>
         </div>
       )}
-      {boost?.action && (
+      {boost.action && (
         <div className="mt-6">
           <p>Action</p>
           <p className="text-muted-foreground break-all">{boost.action}</p>
         </div>
       )}
-      {boost?.ts && (
+      {boost.ts && (
         <div className="mt-6">
           <p>Timestamp</p>
           <p className="text-muted-foreground break-all">{boost.ts}</p>
         </div>
       )}
-      {boost?.value_msat_total && (
+      {boost.value_msat_total && (
         <div className="mt-6">
           <p>Total amount</p>
           <p className="text-muted-foreground break-all">
@@ -44,13 +44,13 @@ function PodcastingInfo({ boost }: { boost: Boostagram }) {
           </p>
         </div>
       )}
-      {boost?.sender_name && (
+      {boost.sender_name && (
         <div className="mt-6">
           <p>Sender</p>
           <p className="text-muted-foreground break-all">{boost.sender_name}</p>
         </div>
       )}
-      {boost?.app_name && (
+      {boost.app_name && (
         <div className="mt-6">
           <p>App</p>
           <p className="text-muted-foreground break-all">{boost.app_name}</p>
