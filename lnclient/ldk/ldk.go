7270d4af81d853014cb4773dbc797104e4a5faad
René Aaron <100827540+reneaaron@users.noreply.github.com>
2024-07-30 16:46:08 +0200

    fix: keep anchor reserves when withdrawing savings balance (#363)
    * fix: keep anchor reserves when withdrawing savings balance
    
    * chore: add TODO to improve LDK redeem onchain funds spendable balance calculation
    
    ---------
    
    Co-authored-by: Roland Bewick <roland.bewick@gmail.com>

--- lnclient/ldk/ldk.go
+++ lnclient/ldk/ldk.go
@@ -999,9 +999,12 @@ func (ls *LDKService) GetOnchainBalance(ctx context.Context) (*lnclient.OnchainB
 }
 
 func (ls *LDKService) RedeemOnchainFunds(ctx context.Context, toAddress string) (string, error) {
-	txId, err := ls.node.OnchainPayment().SendAllToAddress(toAddress)
+	spendableBalance := ls.node.ListBalances().SpendableOnchainBalanceSats
+	// TODO: estimate the transaction fee and subtract that from the spendable balance
+	// to avoid spending any of the reserved anchor channel balance
+	txId, err := ls.node.OnchainPayment().SendToAddress(toAddress, spendableBalance)
 	if err != nil {
-		logger.Logger.WithError(err).Error("SendAllToOnchainAddress failed")
+		logger.Logger.WithError(err).Error("SendToAddress failed")
 		return "", err
 	}
 	return txId, nil
