9f45d42d0c619c5d347ddabdc9b3e156db0abe0d
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2024-08-02 12:04:36 -0700

    pwa-config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,48 @@
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        short_name: 'Alby Hub',
+        name: 'Alby Hub',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml'
+          }
+        ],
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#ffffff'
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+      }
+    })
+  ],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:8080",
+      '/api': {
+        target: 'http://localhost:8080',
         secure: false,
       },
     },
   },
   resolve: {
     alias: {
-      src: path.resolve(__dirname, "./src"),
-      wailsjs: path.resolve(__dirname, "./wailsjs"),
+      src: path.resolve(__dirname, './src'),
+      wailsjs: path.resolve(__dirname, './wailsjs'),
     },
   },
 });
