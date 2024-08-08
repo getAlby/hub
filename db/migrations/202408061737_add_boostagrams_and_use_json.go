7c29b25dfb5a0603ed6b46b5840855c0f662cd49
im-adithya <imadithyavardhan@gmail.com>
2024-08-08 20:41:01 +0530

    chore: set metadata json column to null if empty

--- db/migrations/202408061737_add_boostagrams_and_use_json.go
+++ db/migrations/202408061737_add_boostagrams_and_use_json.go
@@ -15,7 +15,7 @@ var _202408061737_add_boostagrams_and_use_json = &gormigrate.Migration{
 			return tx.Exec(`
 			ALTER TABLE transactions ADD COLUMN boostagram JSONB;
 			ALTER TABLE transactions ADD COLUMN metadata_temp JSONB;
-			UPDATE transactions SET metadata_temp = "";
+			UPDATE transactions SET metadata_temp = NULL;
 			UPDATE transactions SET metadata_temp = json(metadata) where metadata != "";
 			ALTER TABLE transactions DROP COLUMN metadata;
 			ALTER TABLE transactions RENAME COLUMN metadata_temp TO metadata;
