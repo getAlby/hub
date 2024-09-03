274f9372018df0c4fdf83608008e55377070b15b
Roland <33993199+rolznz@users.noreply.github.com>
2024-09-03 13:04:43 +0700

    feat: show channel reserves alert on wallet page (#582)
    * feat: show channel reserves alert on wallet page
    
    * chore: only show low receiving capacity alert if migrate card not visible
    
    * feat: different alert on wallet page if user has no channels

--- frontend/src/screens/wallet/index.tsx
+++ frontend/src/screens/wallet/index.tsx
@@ -75,27 +75,50 @@ function Wallet() {
           </div>
         </div>
       )}
-      {hasChannelManagement && !balances.lightning.totalSpendable && (
+      {hasChannelManagement &&
+        !!channels?.length &&
+        channels?.every(
+          (channel) =>
+            channel.localBalance < channel.unspendablePunishmentReserve
+        ) &&
+        !showMigrateCard && (
+          <Alert>
+            <AlertTriangle className="h-4 w-4" />
+            <AlertTitle>Channel Reserves Unmet</AlertTitle>
+            <AlertDescription>
+              You won't be able to make payments until you fill your channel
+              reserve.{" "}
+              <Link to="/channels" className="underline">
+                View channel reserves
+              </Link>
+            </AlertDescription>
+          </Alert>
+        )}
+      {hasChannelManagement &&
+        !!channels?.length &&
+        !balances.lightning.totalReceivable &&
+        !showMigrateCard && (
+          <Alert>
+            <AlertTriangle className="h-4 w-4" />
+            <AlertTitle>Low receiving capacity</AlertTitle>
+            <AlertDescription>
+              You won't be able to receive payments until you{" "}
+              <Link className="underline" to="/channels/incoming">
+                increase your receiving capacity.
+              </Link>
+            </AlertDescription>
+          </Alert>
+        )}
+      {hasChannelManagement && !channels?.length && !showMigrateCard && (
         <Alert>
           <AlertTriangle className="h-4 w-4" />
-          <AlertTitle>Low spending balance</AlertTitle>
+          <AlertTitle>Open Your First Channel</AlertTitle>
           <AlertDescription>
-            You won't be able to make payments until you{" "}
-            <Link className="underline" to="/channels/outgoing">
-              increase your spending balance.
+            You won't be able to receive or send payments until you{" "}
+            <Link className="underline" to="/channels/first">
+              open your first channel
             </Link>
-          </AlertDescription>
-        </Alert>
-      )}
-      {hasChannelManagement && !balances.lightning.totalReceivable && (
-        <Alert>
-          <AlertTriangle className="h-4 w-4" />
-          <AlertTitle>Low receiving capacity</AlertTitle>
-          <AlertDescription>
-            You won't be able to receive payments until you{" "}
-            <Link className="underline" to="/channels/incoming">
-              increase your receiving capacity.
-            </Link>
+            .
           </AlertDescription>
         </Alert>
       )}
