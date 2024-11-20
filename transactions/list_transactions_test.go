771792f570ca99e38472006caec46245f64014b6
Roland Bewick <roland.bewick@gmail.com>
2024-11-19 20:09:54 -0600

    fix: tests

--- transactions/list_transactions_test.go
+++ transactions/list_transactions_test.go
@@ -47,7 +47,7 @@ func TestListTransactions_Paid(t *testing.T) {
 
 	transactionsService := NewTransactionsService(svc.DB, svc.EventPublisher)
 
-	incomingTransactions, err := transactionsService.ListTransactions(ctx, 0, 0, 0, 0, false, false, nil, svc.LNClient, nil)
+	incomingTransactions, err := transactionsService.ListTransactions(ctx, 0, 0, 0, 0, false, false, nil, svc.LNClient, nil, false)
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(incomingTransactions))
 	assert.Equal(t, uint64(123000), incomingTransactions[0].AmountMsat)
@@ -112,7 +112,7 @@ func TestListTransactions_UnpaidIncoming(t *testing.T) {
 
 	transactionsService := NewTransactionsService(svc.DB, svc.EventPublisher)
 
-	incomingTransactions, err := transactionsService.ListTransactions(ctx, 0, 0, 0, 0, false, true, nil, svc.LNClient, nil)
+	incomingTransactions, err := transactionsService.ListTransactions(ctx, 0, 0, 0, 0, false, true, nil, svc.LNClient, nil, false)
 	assert.NoError(t, err)
 	assert.Equal(t, 3, len(incomingTransactions))
 	assert.Equal(t, constants.TRANSACTION_STATE_SETTLED, incomingTransactions[0].State)
@@ -179,7 +179,7 @@ func TestListTransactions_UnpaidOutgoing(t *testing.T) {
 
 	transactionsService := NewTransactionsService(svc.DB, svc.EventPublisher)
 
-	outgoingTransactions, err := transactionsService.ListTransactions(ctx, 0, 0, 0, 0, true, false, nil, svc.LNClient, nil)
+	outgoingTransactions, err := transactionsService.ListTransactions(ctx, 0, 0, 0, 0, true, false, nil, svc.LNClient, nil, false)
 	assert.NoError(t, err)
 	assert.Equal(t, 3, len(outgoingTransactions))
 	assert.Equal(t, constants.TRANSACTION_STATE_SETTLED, outgoingTransactions[0].State)
@@ -246,7 +246,7 @@ func TestListTransactions_Unpaid(t *testing.T) {
 
 	transactionsService := NewTransactionsService(svc.DB, svc.EventPublisher)
 
-	outgoingTransactions, err := transactionsService.ListTransactions(ctx, 0, 0, 0, 0, true, true, nil, svc.LNClient, nil)
+	outgoingTransactions, err := transactionsService.ListTransactions(ctx, 0, 0, 0, 0, true, true, nil, svc.LNClient, nil, false)
 	assert.NoError(t, err)
 	assert.Equal(t, 5, len(outgoingTransactions))
 }
@@ -281,7 +281,7 @@ func TestListTransactions_Limit(t *testing.T) {
 
 	transactionsService := NewTransactionsService(svc.DB, svc.EventPublisher)
 
-	incomingTransactions, err := transactionsService.ListTransactions(ctx, 0, 0, 1, 0, false, false, nil, svc.LNClient, nil)
+	incomingTransactions, err := transactionsService.ListTransactions(ctx, 0, 0, 1, 0, false, false, nil, svc.LNClient, nil, false)
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(incomingTransactions))
 	assert.Equal(t, "first", incomingTransactions[0].Description)
@@ -337,7 +337,7 @@ func TestListTransactions_Offset(t *testing.T) {
 
 	transactionsService := NewTransactionsService(svc.DB, svc.EventPublisher)
 
-	incomingTransactions, err := transactionsService.ListTransactions(ctx, 0, 0, 1, 2, false, false, nil, svc.LNClient, nil)
+	incomingTransactions, err := transactionsService.ListTransactions(ctx, 0, 0, 1, 2, false, false, nil, svc.LNClient, nil, false)
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(incomingTransactions))
 	assert.Equal(t, "third", incomingTransactions[0].Description)
@@ -387,7 +387,7 @@ func TestListTransactions_FromUntil(t *testing.T) {
 
 	transactionsService := NewTransactionsService(svc.DB, svc.EventPublisher)
 
-	incomingTransactions, err := transactionsService.ListTransactions(ctx, uint64(time.Now().Add(4*time.Minute).Unix()), uint64(time.Now().Add(6*time.Minute).Unix()), 0, 0, false, false, nil, svc.LNClient, nil)
+	incomingTransactions, err := transactionsService.ListTransactions(ctx, uint64(time.Now().Add(4*time.Minute).Unix()), uint64(time.Now().Add(6*time.Minute).Unix()), 0, 0, false, false, nil, svc.LNClient, nil, false)
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(incomingTransactions))
 	assert.Equal(t, "second", incomingTransactions[0].Description)
@@ -448,7 +448,7 @@ func TestListTransactions_FromUntilUnpaidOutgoing(t *testing.T) {
 
 	transactionsService := NewTransactionsService(svc.DB, svc.EventPublisher)
 
-	incomingTransactions, err := transactionsService.ListTransactions(ctx, uint64(time.Now().Add(4*time.Minute).Unix()), uint64(time.Now().Add(6*time.Minute).Unix()), 0, 0, true, false, nil, svc.LNClient, nil)
+	incomingTransactions, err := transactionsService.ListTransactions(ctx, uint64(time.Now().Add(4*time.Minute).Unix()), uint64(time.Now().Add(6*time.Minute).Unix()), 0, 0, true, false, nil, svc.LNClient, nil, false)
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(incomingTransactions))
 	assert.Equal(t, "second", incomingTransactions[0].Description)
@@ -510,7 +510,7 @@ func TestListTransactions_FromUntilUnpaidIncoming(t *testing.T) {
 
 	transactionsService := NewTransactionsService(svc.DB, svc.EventPublisher)
 
-	incomingTransactions, err := transactionsService.ListTransactions(ctx, uint64(time.Now().Add(4*time.Minute).Unix()), uint64(time.Now().Add(6*time.Minute).Unix()), 0, 0, false, true, nil, svc.LNClient, nil)
+	incomingTransactions, err := transactionsService.ListTransactions(ctx, uint64(time.Now().Add(4*time.Minute).Unix()), uint64(time.Now().Add(6*time.Minute).Unix()), 0, 0, false, true, nil, svc.LNClient, nil, false)
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(incomingTransactions))
 	assert.Equal(t, "second", incomingTransactions[0].Description)
