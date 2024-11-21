6a0ab24dc015ec311a7822b4ad6606f4c3ba8ca5
Roland Bewick <roland.bewick@gmail.com>
2024-11-21 15:33:42 -0600

    feat: redesign first channel payment UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -85,6 +85,7 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "spin-slow": "spin 3s linear infinite",
       },
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
