087a588ae67dfe607d6404b18d5fbc001228635e
Roland Bewick <roland.bewick@gmail.com>
2024-11-29 15:44:37 -0600

    chore: add list transactions tests

--- nip47/controllers/list_transactions_controller_test.go
+++ nip47/controllers/list_transactions_controller_test.go
@@ -18,7 +18,13 @@ import (
 	"github.com/getAlby/hub/transactions"
 )
 
-const nip47ListTransactionsJson = `
+func TestHandleListTransactionsEvent(t *testing.T) {
+	ctx := context.TODO()
+	defer tests.RemoveTestService()
+	svc, err := tests.CreateTestService()
+	require.NoError(t, err)
+
+	const nip47ListTransactionsJson = `
 {
 	"method": "list_transactions",
 	"params": {
@@ -30,12 +36,6 @@ const nip47ListTransactionsJson = `
 	}
 }
 `
-
-func TestHandleListTransactionsEvent(t *testing.T) {
-	ctx := context.TODO()
-	defer tests.RemoveTestService()
-	svc, err := tests.CreateTestService()
-	require.NoError(t, err)
 
 	nip47Request := &models.Request{}
 	err = json.Unmarshal([]byte(nip47ListTransactionsJson), nip47Request)
@@ -94,6 +94,260 @@ func TestHandleListTransactionsEvent(t *testing.T) {
 	assert.Equal(t, tests.MockLNClientTransactions[0].FeesPaid, transaction.FeesPaid)
 	assert.Equal(t, tests.MockLNClientTransactions[0].SettledAt, transaction.SettledAt)
 	assert.Equal(t, "settled", transaction.State)
+}
+
+func TestHandleListTransactionsEvent_UnpaidOutgoingOnly(t *testing.T) {
+	ctx := context.TODO()
+	defer tests.RemoveTestService()
+	svc, err := tests.CreateTestService()
+	require.NoError(t, err)
+
+	const nip47ListTransactionsJson = `
+{
+	"method": "list_transactions",
+	"params": {
+		"from": 0,
+		"until": 0,
+		"limit": 10,
+		"offset": 0,
+		"unpaid_outgoing": true
+	}
+}
+`
+
+	nip47Request := &models.Request{}
+	err = json.Unmarshal([]byte(nip47ListTransactionsJson), nip47Request)
+	assert.NoError(t, err)
+
+	app, _, err := tests.CreateApp(svc)
+	assert.NoError(t, err)
+
+	dbRequestEvent := &db.RequestEvent{
+		AppId: &app.ID,
+	}
+	err = svc.DB.Create(&dbRequestEvent).Error
+	assert.NoError(t, err)
+
+	err = svc.DB.Create(&db.Transaction{
+		Type:  constants.TRANSACTION_TYPE_INCOMING,
+		State: constants.TRANSACTION_STATE_PENDING,
+	}).Error
+	assert.NoError(t, err)
+
+	err = svc.DB.Create(&db.Transaction{
+		Type:  constants.TRANSACTION_TYPE_OUTGOING,
+		State: constants.TRANSACTION_STATE_PENDING,
+	}).Error
+	assert.NoError(t, err)
+
+	var publishedResponse *models.Response
+
+	publishResponse := func(response *models.Response, tags nostr.Tags) {
+		publishedResponse = response
+	}
+
+	permissionsSvc := permissions.NewPermissionsService(svc.DB, svc.EventPublisher)
+	transactionsSvc := transactions.NewTransactionsService(svc.DB, svc.EventPublisher)
+	NewNip47Controller(svc.LNClient, svc.DB, svc.EventPublisher, permissionsSvc, transactionsSvc).
+		HandleListTransactionsEvent(ctx, nip47Request, dbRequestEvent.ID, *dbRequestEvent.AppId, publishResponse)
+
+	assert.Nil(t, publishedResponse.Error)
+
+	assert.Equal(t, 1, len(publishedResponse.Result.(*listTransactionsResponse).Transactions))
+	transaction := publishedResponse.Result.(*listTransactionsResponse).Transactions[0]
+	assert.Equal(t, constants.TRANSACTION_TYPE_OUTGOING, transaction.Type)
+}
+
+func TestHandleListTransactionsEvent_UnpaidIncomingOnly(t *testing.T) {
+	ctx := context.TODO()
+	defer tests.RemoveTestService()
+	svc, err := tests.CreateTestService()
+	require.NoError(t, err)
+
+	const nip47ListTransactionsJson = `
+{
+	"method": "list_transactions",
+	"params": {
+		"from": 0,
+		"until": 0,
+		"limit": 10,
+		"offset": 0,
+		"unpaid_incoming": true
+	}
+}
+`
+
+	nip47Request := &models.Request{}
+	err = json.Unmarshal([]byte(nip47ListTransactionsJson), nip47Request)
+	assert.NoError(t, err)
+
+	app, _, err := tests.CreateApp(svc)
+	assert.NoError(t, err)
+
+	dbRequestEvent := &db.RequestEvent{
+		AppId: &app.ID,
+	}
+	err = svc.DB.Create(&dbRequestEvent).Error
+	assert.NoError(t, err)
+
+	err = svc.DB.Create(&db.Transaction{
+		Type:  constants.TRANSACTION_TYPE_INCOMING,
+		State: constants.TRANSACTION_STATE_PENDING,
+	}).Error
+	assert.NoError(t, err)
+
+	err = svc.DB.Create(&db.Transaction{
+		Type:  constants.TRANSACTION_TYPE_OUTGOING,
+		State: constants.TRANSACTION_STATE_PENDING,
+	}).Error
+	assert.NoError(t, err)
+
+	var publishedResponse *models.Response
+
+	publishResponse := func(response *models.Response, tags nostr.Tags) {
+		publishedResponse = response
+	}
+
+	permissionsSvc := permissions.NewPermissionsService(svc.DB, svc.EventPublisher)
+	transactionsSvc := transactions.NewTransactionsService(svc.DB, svc.EventPublisher)
+	NewNip47Controller(svc.LNClient, svc.DB, svc.EventPublisher, permissionsSvc, transactionsSvc).
+		HandleListTransactionsEvent(ctx, nip47Request, dbRequestEvent.ID, *dbRequestEvent.AppId, publishResponse)
+
+	assert.Nil(t, publishedResponse.Error)
+
+	assert.Equal(t, 1, len(publishedResponse.Result.(*listTransactionsResponse).Transactions))
+	transaction := publishedResponse.Result.(*listTransactionsResponse).Transactions[0]
+	assert.Equal(t, constants.TRANSACTION_TYPE_INCOMING, transaction.Type)
+}
+
+func TestHandleListTransactionsEvent_Unpaid(t *testing.T) {
+	ctx := context.TODO()
+	defer tests.RemoveTestService()
+	svc, err := tests.CreateTestService()
+	require.NoError(t, err)
+
+	const nip47ListTransactionsJson = `
+{
+	"method": "list_transactions",
+	"params": {
+		"from": 0,
+		"until": 0,
+		"limit": 10,
+		"offset": 0,
+		"unpaid": true
+	}
+}
+`
+
+	nip47Request := &models.Request{}
+	err = json.Unmarshal([]byte(nip47ListTransactionsJson), nip47Request)
+	assert.NoError(t, err)
+
+	app, _, err := tests.CreateApp(svc)
+	assert.NoError(t, err)
+
+	dbRequestEvent := &db.RequestEvent{
+		AppId: &app.ID,
+	}
+	err = svc.DB.Create(&dbRequestEvent).Error
+	assert.NoError(t, err)
+
+	err = svc.DB.Create(&db.Transaction{
+		Type:  constants.TRANSACTION_TYPE_INCOMING,
+		State: constants.TRANSACTION_STATE_PENDING,
+	}).Error
+	assert.NoError(t, err)
+
+	err = svc.DB.Create(&db.Transaction{
+		Type:  constants.TRANSACTION_TYPE_OUTGOING,
+		State: constants.TRANSACTION_STATE_PENDING,
+	}).Error
+	assert.NoError(t, err)
+
+	var publishedResponse *models.Response
+
+	publishResponse := func(response *models.Response, tags nostr.Tags) {
+		publishedResponse = response
+	}
+
+	permissionsSvc := permissions.NewPermissionsService(svc.DB, svc.EventPublisher)
+	transactionsSvc := transactions.NewTransactionsService(svc.DB, svc.EventPublisher)
+	NewNip47Controller(svc.LNClient, svc.DB, svc.EventPublisher, permissionsSvc, transactionsSvc).
+		HandleListTransactionsEvent(ctx, nip47Request, dbRequestEvent.ID, *dbRequestEvent.AppId, publishResponse)
+
+	assert.Nil(t, publishedResponse.Error)
+
+	assert.Equal(t, 2, len(publishedResponse.Result.(*listTransactionsResponse).Transactions))
+}
+
+func TestHandleListTransactionsEvent_Paid(t *testing.T) {
+	ctx := context.TODO()
+	defer tests.RemoveTestService()
+	svc, err := tests.CreateTestService()
+	require.NoError(t, err)
+
+	const nip47ListTransactionsJson = `
+{
+	"method": "list_transactions",
+	"params": {
+		"from": 0,
+		"until": 0,
+		"limit": 10,
+		"offset": 0
+	}
+}
+`
+
+	nip47Request := &models.Request{}
+	err = json.Unmarshal([]byte(nip47ListTransactionsJson), nip47Request)
+	assert.NoError(t, err)
+
+	app, _, err := tests.CreateApp(svc)
+	assert.NoError(t, err)
+
+	dbRequestEvent := &db.RequestEvent{
+		AppId: &app.ID,
+	}
+	err = svc.DB.Create(&dbRequestEvent).Error
+	assert.NoError(t, err)
+
+	err = svc.DB.Create(&db.Transaction{
+		Type:  constants.TRANSACTION_TYPE_INCOMING,
+		State: constants.TRANSACTION_STATE_PENDING,
+	}).Error
+	assert.NoError(t, err)
+
+	err = svc.DB.Create(&db.Transaction{
+		Type:  constants.TRANSACTION_TYPE_OUTGOING,
+		State: constants.TRANSACTION_STATE_PENDING,
+	}).Error
+	assert.NoError(t, err)
+
+	settledPaymentHash := "dummy payment hash"
+
+	err = svc.DB.Create(&db.Transaction{
+		Type:        constants.TRANSACTION_TYPE_OUTGOING,
+		State:       constants.TRANSACTION_STATE_SETTLED,
+		PaymentHash: settledPaymentHash,
+	}).Error
+	assert.NoError(t, err)
+
+	var publishedResponse *models.Response
+
+	publishResponse := func(response *models.Response, tags nostr.Tags) {
+		publishedResponse = response
+	}
+
+	permissionsSvc := permissions.NewPermissionsService(svc.DB, svc.EventPublisher)
+	transactionsSvc := transactions.NewTransactionsService(svc.DB, svc.EventPublisher)
+	NewNip47Controller(svc.LNClient, svc.DB, svc.EventPublisher, permissionsSvc, transactionsSvc).
+		HandleListTransactionsEvent(ctx, nip47Request, dbRequestEvent.ID, *dbRequestEvent.AppId, publishResponse)
+
+	assert.Nil(t, publishedResponse.Error)
+
+	assert.Equal(t, 1, len(publishedResponse.Result.(*listTransactionsResponse).Transactions))
+	transaction := publishedResponse.Result.(*listTransactionsResponse).Transactions[0]
+	assert.Equal(t, settledPaymentHash, transaction.PaymentHash)
 }
 
 // TODO: add tests for pagination args
