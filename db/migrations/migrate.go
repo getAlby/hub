e5b2113f4a40220f877af1adfb063b306e81f0af
Roman Dmitrienko <roman@dmitrienko.com>
2024-12-22 01:13:51 +0300

    feat: implement migration templates

--- db/migrations/migrate.go
+++ db/migrations/migrate.go
@@ -1,6 +1,10 @@
 package migrations
 
 import (
+	"fmt"
+	"strings"
+	"text/template"
+
 	"github.com/go-gormigrate/gormigrate/v2"
 	"gorm.io/gorm"
 )
@@ -26,4 +30,42 @@ func Migrate(gormDB *gorm.DB) error {
 	})
 
 	return m.Migrate()
+}
+
+type sqlDialectDef struct {
+	Timestamp               string
+	AutoincrementPrimaryKey string
+	DropTableCascade        string
+}
+
+var sqlDialectSqlite = sqlDialectDef{
+	Timestamp:               "datetime",
+	AutoincrementPrimaryKey: "INTEGER PRIMARY KEY AUTOINCREMENT",
+	DropTableCascade:        "",
+}
+
+var sqlDialectPostgres = sqlDialectDef{
+	Timestamp:               "timestamp",
+	AutoincrementPrimaryKey: "SERIAL PRIMARY KEY",
+	DropTableCascade:        "CASCADE",
+}
+
+func getDialect(tx *gorm.DB) *sqlDialectDef {
+	switch tx.Dialector.Name() {
+	case "postgres":
+		return &sqlDialectPostgres
+	default:
+		return &sqlDialectSqlite
+	}
+}
+
+func exec(tx *gorm.DB, templ *template.Template) error {
+	dialect := getDialect(tx)
+	var buf strings.Builder
+	err := templ.Execute(&buf, dialect)
+	if err != nil {
+		panic(fmt.Sprintf("failed to render SQL template: %v", err))
+	}
+
+	return tx.Exec(buf.String()).Error
 }
