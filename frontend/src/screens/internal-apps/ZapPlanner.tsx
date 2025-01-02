78e48cc50bb30f7921f57411f54e3bee013b9813
René Aaron <rene@twentyuno.net>
2025-01-02 17:25:54 +0100

    feat: dialog

--- frontend/src/screens/internal-apps/ZapPlanner.tsx
+++ frontend/src/screens/internal-apps/ZapPlanner.tsx
@@ -4,21 +4,33 @@ import AppCard from "src/components/connections/AppCard";
 import {
   Card,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "src/components/ui/card";
-import { LoadingButton } from "src/components/ui/loading-button";
 import { useToast } from "src/components/ui/use-toast";
 import { useApps } from "src/hooks/useApps";
 import { createApp } from "src/requests/createApp";
 import { CreateAppRequest } from "src/types";
 import { handleRequestError } from "src/utils/handleRequestError";
 
+import { PlusCircle } from "lucide-react";
 import alby from "src/assets/suggested-apps/alby.png";
 import hrf from "src/assets/zapplanner/hrf.png";
 import opensats from "src/assets/zapplanner/opensats.png";
+import ExternalLink from "src/components/ExternalLink";
+import { Button } from "src/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "src/components/ui/dialog";
 import { Input } from "src/components/ui/input";
+import { Label } from "src/components/ui/label";
+import { LoadingButton } from "src/components/ui/loading-button";
 
 type Recipient = {
   name: string;
@@ -54,16 +66,16 @@ const recipients: Recipient[] = [
 export function ZapPlanner() {
   const { data: apps, mutate: reloadApps } = useApps();
   const { toast } = useToast();
-  const [submittingFor, setSubmittingFor] = React.useState("");
+
+  const [open, setOpen] = React.useState(false);
+  const [isSubmitting, setSubmittingFor] = React.useState("");
   const [customName, setCustomName] = React.useState("");
   const [customLightningAddress, setCustomLightningAddress] =
     React.useState("");
 
-  const handleSubmit = async (name: string, lightningAddress: string) => {
-    setSubmittingFor(lightningAddress);
-
+  const handleSubmit = async () => {
     try {
-      if (apps?.some((existingApp) => existingApp.name === name)) {
+      if (apps?.some((existingApp) => existingApp.name === customName)) {
         throw new Error("A connection with the same name already exists.");
       }
 
@@ -85,7 +97,7 @@ export function ZapPlanner() {
         isolated: false,
         metadata: {
           app_store_app_id: "zapplanner",
-          recipient_lightning_address: lightningAddress,
+          recipient_lightning_address: customLightningAddress,
         },
       };
 
@@ -99,7 +111,7 @@ export function ZapPlanner() {
       // TODO: consider fiat conversion
       // TODO: consider using the ZapPlanner API to make the connection, then the subscription ID can be saved in the app metadata
       window.open(
-        `https://zapplanner.albylabs.com/confirm?amount=${amountSats}&recipient=${lightningAddress}&timeframe=31%20days&comment=${comment}&payerdata=${payerData}&nwcUrl=${encodeURIComponent(createAppResponse.pairingUri)}`,
+        `https://zapplanner.albylabs.com/confirm?amount=${amountSats}&recipient=${customLightningAddress}&timeframe=31%20days&comment=${comment}&payerdata=${payerData}&nwcUrl=${encodeURIComponent(createAppResponse.pairingUri)}`,
         "_blank"
       );
 
@@ -119,62 +131,102 @@ export function ZapPlanner() {
       <AppHeader
         title="ZapPlanner"
         description="Schedule automatic recurring lightning payments"
+        contentRight={
+          <>
+            <Dialog open={open} onOpenChange={setOpen}>
+              <DialogTrigger asChild>
+                <Button>
+                  <PlusCircle className="h-4 w-4 mr-2" />
+                  New Recurring Payment
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[600px]">
+                <DialogHeader>
+                  <DialogTitle>New Recurring Payment</DialogTitle>
+                  <DialogDescription>
+                    For advanced options go to{" "}
+                    <ExternalLink
+                      className="underline"
+                      to="https://zapplanner.albylabs.com"
+                    >
+                      zapplanner.albylabs.com
+                    </ExternalLink>{" "}
+                    and create your recurring payment there.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Name
+                    </Label>
+                    <Input
+                      id="name"
+                      value={customName}
+                      className="col-span-3"
+                    />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Receiver
+                    </Label>
+                    <Input
+                      id="receiver"
+                      value={customLightningAddress}
+                      className="col-span-3"
+                    />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="amount" className="text-right">
+                      Amount
+                    </Label>
+                    <Input id="amount" value={5000} className="col-span-3" />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="comment" className="text-right">
+                      Comment
+                    </Label>
+                    <Input id="comment" value="" className="col-span-3" />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <LoadingButton
+                    type="submit"
+                    onClick={() => handleSubmit()}
+                    disabled={!!isSubmitting}
+                    loading={isSubmitting}
+                  >
+                    Create
+                  </LoadingButton>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </>
+        }
       />
-      {/* TODO: add a dialog so a comment can be passed */}
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-3">
         {recipients.map((recipient) => (
           <Card key={recipient.lightningAddress}>
-            <CardHeader className="h-32">
+            <CardHeader>
               <div className="flex flex-row items-center gap-3">
                 <img src={recipient.logo} className="rounded-lg w-10 h-10" />
-                <CardTitle>{recipient.name}</CardTitle>
+                <div className="flex flex-row gap-3 grow justify-between items-center">
+                  <CardTitle>{recipient.name}</CardTitle>
+                  <LoadingButton
+                    size="sm"
+                    onClick={() => {
+                      setCustomName(recipient.name);
+                      setCustomLightningAddress(recipient.lightningAddress);
+                      setOpen(true);
+                    }}
+                  >
+                    Support
+                  </LoadingButton>
+                </div>
               </div>
               <CardDescription>{recipient.description}</CardDescription>
             </CardHeader>
-            <CardFooter className="flex justify-end">
-              <LoadingButton
-                disabled={!!submittingFor}
-                size="sm"
-                loading={submittingFor === recipient.lightningAddress}
-                onClick={() => {
-                  handleSubmit(recipient.name, recipient.lightningAddress);
-                }}
-              >
-                Support with 5000 sats / month
-              </LoadingButton>
-            </CardFooter>
           </Card>
         ))}
-        <Card>
-          <CardHeader className="h-32">
-            <CardTitle>Custom</CardTitle>
-            <CardDescription className="flex flex-col gap-2">
-              <Input
-                value={customName}
-                onChange={(e) => setCustomName(e.target.value)}
-                placeholder={"Name"}
-              />
-              <Input
-                value={customLightningAddress}
-                onChange={(e) => setCustomLightningAddress(e.target.value)}
-                placeholder={"lightning@address.com"}
-              />
-            </CardDescription>
-          </CardHeader>
-          <CardFooter className="flex justify-end mt-2">
-            <LoadingButton
-              disabled={
-                !!submittingFor || !customName || !customLightningAddress
-              }
-              loading={!!submittingFor}
-              onClick={() => {
-                handleSubmit(customName, customLightningAddress);
-              }}
-            >
-              Support with 5000 sats / month
-            </LoadingButton>
-          </CardFooter>
-        </Card>
       </div>
 
       {!!zapplannerApps?.length && (
