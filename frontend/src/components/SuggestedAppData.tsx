50b171d4e4c7e4d07747a418e46f6fd4e28de79b
Roland Bewick <roland.bewick@gmail.com>
2025-01-14 21:39:41 +0700

    fix: prettier formatting

--- frontend/src/components/SuggestedAppData.tsx
+++ frontend/src/components/SuggestedAppData.tsx
@@ -607,25 +607,21 @@ export const suggestedApps: SuggestedApp[] = [
           <h3 className="font-medium">In Nostrcheck server</h3>
           <ul className="list-inside text-muted-foreground">
             <li>
-              1. Go to {" "}
+              1. Go to{" "}
               <span className="font-medium text-foreground">Settings</span> and
               choose{" "}
-              <span className="font-medium text-foreground">
-                Payments
-              </span>
-              {" "}tab
+              <span className="font-medium text-foreground">Payments</span> tab
             </li>
             <li>
-              2. Scroll to Nostr wallet connect settings and paste the {" "}
-              <span className="font-medium text-foreground">connection secret</span>
-              {" "}from Alby Hub
+              2. Scroll to Nostr wallet connect settings and paste the{" "}
+              <span className="font-medium text-foreground">
+                connection secret
+              </span>{" "}
+              from Alby Hub
             </li>
             <li>
-              3. Press the {" "}
-              <span className="font-medium text-foreground">
-                Save 
-              </span>
-              {" "}button
+              3. Press the{" "}
+              <span className="font-medium text-foreground">Save</span> button
             </li>
           </ul>
         </div>
