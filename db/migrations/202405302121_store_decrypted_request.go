d80b641564b88fddfaf49df2b679f2bb2feca7e0
Roman Dmitrienko <roman@dmitrienko.com>
2025-01-14 09:01:35 +0300

    fix: address PR feedback

--- db/migrations/202405302121_store_decrypted_request.go
+++ db/migrations/202405302121_store_decrypted_request.go
@@ -11,17 +11,17 @@ var _202405302121_store_decrypted_request = &gormigrate.Migration{
 	ID: "_202405302121_store_decrypted_request",
 	Migrate: func(tx *gorm.DB) error {
 
-		err := tx.Exec("ALTER TABLE request_events ADD COLUMN method TEXT;").Error
+		err := tx.Exec(`ALTER TABLE request_events ADD COLUMN method TEXT;`).Error
 		if err != nil {
 			return err
 		}
 
-		err = tx.Exec("CREATE INDEX \"idx_request_events_method\" ON \"request_events\"(\"method\");").Error
+		err = tx.Exec(`CREATE INDEX "idx_request_events_method" ON "request_events"("method");`).Error
 		if err != nil {
 			return err
 		}
 
-		err = tx.Exec("ALTER TABLE request_events ADD COLUMN content_data TEXT").Error
+		err = tx.Exec(`ALTER TABLE request_events ADD COLUMN content_data TEXT`).Error
 		return err
 	},
 	Rollback: func(tx *gorm.DB) error {
