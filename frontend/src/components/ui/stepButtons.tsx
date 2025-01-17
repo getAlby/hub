e6c2ae1b7316ee1ab5c6ccedddd5c946490b957e
pavanjoshi914 <pavanj914@gmail.com>
2025-01-17 15:34:23 +0530

    feat: previous and back step buttons

--- frontend/src/components/ui/stepButtons.tsx
+++ frontend/src/components/ui/stepButtons.tsx
@@ -2,13 +2,24 @@ import { Button } from "src/components/ui/button";
 import { useStepper } from "src/components/ui/Stepper";
 
 export default function StepButtons() {
-  const { nextStep } = useStepper();
-
+  const { nextStep, prevStep, isLastStep, isDisabledStep } = useStepper();
   return (
-    <div className="w-full flex mt-4 mb-4">
-      <Button size="sm" onClick={nextStep}>
-        Next
-      </Button>
-    </div>
+    <>
+      <div className="w-full flex justify-start gap-2 mt-8 mb-2">
+        <>
+          {isLastStep ? (
+            <Button
+              disabled={isDisabledStep}
+              onClick={prevStep}
+              variant="secondary"
+            >
+              Back
+            </Button>
+          ) : (
+            <Button onClick={nextStep}>Next</Button>
+          )}
+        </>
+      </div>
+    </>
   );
 }
