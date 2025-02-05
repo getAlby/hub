24ea41b4ebc21bf9cf17a3a62c4021844d5da11c
René Aaron <rene@twentyuno.net>
2025-02-05 14:50:53 +0100

    fix: update copy, preset amounts

--- frontend/src/screens/alby/SupportAlby.tsx
+++ frontend/src/screens/alby/SupportAlby.tsx
@@ -1,7 +1,16 @@
 import { CheckCircle2, Code, PlusCircle, RefreshCw } from "lucide-react";
 import React from "react";
 import ExternalLink from "src/components/ExternalLink";
-import { AlertDialog, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "src/components/ui/alert-dialog";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "src/components/ui/alert-dialog";
 import { Button } from "src/components/ui/button";
 import {
   Card,
@@ -25,7 +34,7 @@ const recipientLightningAddress = "hub@getalby.com";
 
 function SupportAlby() {
   const { data: apps, mutate: reloadApps } = useApps();
-  const subscription = apps?.find(x => x.name === appName);
+  const subscription = apps?.find((x) => x.name === appName);
   const { toast } = useToast();
   const { data: transactions, isLoading } = useTransactions(subscription?.id);
 
@@ -131,22 +140,22 @@ function SupportAlby() {
     }
   };
 
-
   return (
     <>
       <div className="h-full w-full max-w-screen-sm mx-auto flex flex-col justify-center">
         <div className="flex flex-col items-center justify-center gap-6">
-          {!subscription &&
+          {!subscription && (
             <>
               <section className="text-center">
                 <h2 className="text-3xl font-semibold mb-2">
                   ✨ Your Support Matters
                 </h2>
                 <p className="text-muted-foreground">
-                  Our open-source Lightning node is dedicated to enhancing the
-                  Bitcoin ecosystem by providing a reliable, efficient, and
-                  user-friendly platform for transactions. With your support, we can
-                  continue to innovate and expand our services.
+                  We are committed to elevating the Bitcoin ecosystem by
+                  offering reliable, efficient, and user-friendly software
+                  solutions for seamless transactions. With your help, we can
+                  keep pushing boundaries and evolving Alby Hub into something
+                  even more extraordinary.
                 </p>
               </section>
               <Card className="w-full">
@@ -154,38 +163,58 @@ function SupportAlby() {
                   <CardTitle>Why Your Contribution Is Important</CardTitle>
                 </CardHeader>
                 <CardContent>
-                  <ul className="flex flex-col gap-2">
-                    <li className="flex flex-col ">
+                  <ul className="flex flex-col gap-5">
+                    <li className="flex flex-col">
                       <div className="flex flex-row items-center">
                         <PlusCircle className="w-4 h-4 mr-2" />
-                        New Features
+                        Unlock New Features
                       </div>
-                      <div className="text-muted-foreground">
-                        Your support allows us to build and integrate new
-                        <ExternalLink className="underline" to="https://github.com/getAlby/hub/issues">
+                      <div className="text-muted-foreground text-sm">
+                        Your support empowers us to design and implement
+                        cutting-edge{" "}
+                        <ExternalLink
+                          className="underline"
+                          to="https://github.com/getAlby/hub/issues"
+                        >
                           features
-                        </ExternalLink>
+                        </ExternalLink>{" "}
+                        that enhance your experience and keep us at the
+                        forefront of technology.
                       </div>
                     </li>
                     <li className="flex flex-col ">
                       <div className="flex flex-row items-center">
                         <RefreshCw className="w-4 h-4 mr-2" />
-                        Regular Updates
+                        Ensure Continuous Improvement
                       </div>
-                      <div className="text-muted-foreground">
-                        Contributions fund ongoing maintenance and
-                        <ExternalLink className="underline" to="https://github.com/getAlby/hub/releases">
-                          improvements
-                        </ExternalLink>
+                      <div className="text-muted-foreground text-sm">
+                        With your contributions, we can provide{" "}
+                        <ExternalLink
+                          className="underline"
+                          to="https://github.com/getAlby/hub/releases"
+                        >
+                          regular updates
+                        </ExternalLink>{" "}
+                        and ongoing maintenance, ensuring everything runs
+                        smoothly and efficiently for all users.
                       </div>
                     </li>
                     <li className="flex flex-col ">
                       <div className="flex flex-row items-center">
                         <Code className="w-4 h-4 mr-2" />
-                        Open-Source
+                        Support Open-Source Freedom
                       </div>
-                      <div className="text-muted-foreground">
-                        Keep Alby Hub open-source and free for all users
+                      <div className="text-muted-foreground text-sm">
+                        Your support helps us keep Alby Hub true to the
+                        principles of{" "}
+                        <ExternalLink
+                          className="underline"
+                          to="https://github.com/getAlby/hub/blob/master/LICENSE"
+                        >
+                          free and open-source software
+                        </ExternalLink>{" "}
+                        and remains accessible for everyone to use, modify and
+                        improve.
                       </div>
                     </li>
                   </ul>
@@ -200,38 +229,64 @@ function SupportAlby() {
                     <AlertDialogHeader>
                       <AlertDialogTitle>Become a Supporter</AlertDialogTitle>
                       <AlertDialogDescription>
-                        A new ZapPlanner app will be created specificially for this purpose and can be cancelled any time.
+                        A new ZapPlanner app will be created specificially for
+                        this purpose and can be cancelled any time.
                       </AlertDialogDescription>
                     </AlertDialogHeader>
 
                     <div className="flex flex-col gap-3 my-5">
                       <div className="grid grid-cols-4 gap-4">
                         <Label htmlFor="amount" className="text-right mt-2">
-                          Amount <br></br><span className="font-normal text-muted-foreground">(sats / month)</span>
+                          Amount <br></br>
+                          <span className="font-normal text-muted-foreground">
+                            (sats / month)
+                          </span>
                         </Label>
                         <div className="col-span-3">
                           <Input
                             id="amount"
                             value={amount}
-                            onChange={(e) => setAmount(e.target.value)} />
+                            onChange={(e) => setAmount(e.target.value)}
+                          />
                           <div className="grid grid-cols-3 gap-3 mt-1">
-                            <Button type="button" variant="outline" onClick={() => setAmount("1000")}>🙏 1000</Button>
-                            <Button type="button" variant="outline" onClick={() => setAmount("5000")}>💪 5000</Button>
-                            <Button type="button" variant="outline" onClick={() => setAmount("10000")}>✨ 10000</Button>
+                            <Button
+                              type="button"
+                              variant="outline"
+                              onClick={() => setAmount("3000")}
+                            >
+                              🙏 3000
+                            </Button>
+                            <Button
+                              type="button"
+                              variant="outline"
+                              onClick={() => setAmount("6000")}
+                            >
+                              💪 6000
+                            </Button>
+                            <Button
+                              type="button"
+                              variant="outline"
+                              onClick={() => setAmount("10000")}
+                            >
+                              ✨ 10000
+                            </Button>
                           </div>
                         </div>
-
                       </div>
                       <div className="grid grid-cols-4 items-center gap-4">
                         <Label htmlFor="comment" className="text-right">
-                          Name <span className="font-normal text-muted-foreground">(optional)</span>
+                          Name{" "}
+                          <span className="font-normal text-muted-foreground">
+                            (optional)
+                          </span>
                         </Label>
                         <Input
                           id="sender-name"
                           value={senderName}
                           onChange={(e) => setSenderName(e.target.value)}
                           placeholder={`Nickname, npub, @twitter, etc.`}
-                          className="col-span-3" />
+                          className="col-span-3"
+                        />
                       </div>
                     </div>
 
@@ -247,23 +302,39 @@ function SupportAlby() {
                     </AlertDialogFooter>
                   </form>
                 </AlertDialogContent>
-              </AlertDialog></>
-          }
-          {subscription && <>
-            <Card className="w-full">
-              <CardHeader>
-                <CardTitle className="flex flex-row justify-between">
-                  <div className="flex">
-                    <CheckCircle2 className="w-4 h-4 mr-2" />
-                    You are an Alby Supporter!
-                  </div>
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <div className="flex flex-row justify-between">
-                  <div>
+              </AlertDialog>
+            </>
+          )}
+          {subscription && (
+            <>
+              <Card className="w-full">
+                <CardHeader>
+                  <CardTitle className="flex flex-row justify-between">
+                    <div className="flex">
+                      <CheckCircle2 className="w-4 h-4 mr-2" />
+                      You are an Alby Supporter!
+                    </div>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex flex-row justify-between">
                     <div>
-                      <p className="text-muted-foreground text-sm">Total contributions</p>
+                      <div>
+                        <p className="text-muted-foreground text-sm">
+                          Total contributions
+                        </p>
+                        <p className="text-xl font-medium">
+                          {new Intl.NumberFormat().format(
+                            Math.floor(1000000 / 1000)
+                          )}{" "}
+                          sats
+                        </p>
+                      </div>
+                    </div>
+                    <div>
+                      <p className="text-muted-foreground text-sm">
+                        Amount per month
+                      </p>
                       <p className="text-xl font-medium">
                         {new Intl.NumberFormat().format(
                           Math.floor(1000000 / 1000)
@@ -272,21 +343,12 @@ function SupportAlby() {
                       </p>
                     </div>
                   </div>
-                  <div>
-                    <p className="text-muted-foreground text-sm">Amount per month</p>
-                    <p className="text-xl font-medium">
-                      {new Intl.NumberFormat().format(
-                        Math.floor(1000000 / 1000)
-                      )}{" "}
-                      sats
-                    </p>
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
-          </>}
+                </CardContent>
+              </Card>
+            </>
+          )}
         </div>
-      </div >
+      </div>
     </>
   );
 }
