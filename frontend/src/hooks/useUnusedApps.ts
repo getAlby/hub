e0fe3cd7b877e389737257919744bc04d19c2325
Roland Bewick <roland.bewick@gmail.com>
2025-03-04 19:19:09 +0700

    chore: memoize unusedApps

--- frontend/src/hooks/useUnusedApps.ts
+++ frontend/src/hooks/useUnusedApps.ts
@@ -1,11 +1,16 @@
 import dayjs from "dayjs";
+import React from "react";
 import { useApps } from "src/hooks/useApps";
 
+const oldDate = dayjs().subtract(2, "months");
 export function useUnusedApps() {
   const { data: apps } = useApps();
 
-  const oldDate = dayjs().subtract(2, "months");
-  return apps?.filter(
-    (app) => !app.lastEventAt || dayjs(app.lastEventAt).isBefore(oldDate)
+  return React.useMemo(
+    () =>
+      apps?.filter(
+        (app) => !app.lastEventAt || dayjs(app.lastEventAt).isBefore(oldDate)
+      ),
+    [apps]
   );
 }
