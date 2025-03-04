7385e8a0b5820b7d43224a6ae72f85ca9a5761ce
René Aaron <rene@twentyuno.net>
2025-03-04 13:46:28 +0100

    fix: exclude sub-wallets

--- frontend/src/hooks/useUnusedApps.ts
+++ frontend/src/hooks/useUnusedApps.ts
@@ -2,14 +2,17 @@ import dayjs from "dayjs";
 import React from "react";
 import { useApps } from "src/hooks/useApps";
 
-const oldDate = dayjs().subtract(2, "months");
+const OLD_DATE = dayjs().subtract(2, "months");
+
 export function useUnusedApps() {
   const { data: apps } = useApps();
 
   return React.useMemo(
     () =>
       apps?.filter(
-        (app) => !app.lastEventAt || dayjs(app.lastEventAt).isBefore(oldDate)
+        (app) =>
+          (!app.lastEventAt || dayjs(app.lastEventAt).isBefore(OLD_DATE)) &&
+          app.metadata?.app_store_app_id != "uncle-jim"
       ),
     [apps]
   );
