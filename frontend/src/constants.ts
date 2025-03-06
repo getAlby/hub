bebc3557c21f65983d49bf22354b2c84ba2b1c03
René Aaron <rene@twentyuno.net>
2025-03-06 16:16:24 +0100

    fix: make yearly plan the default

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -14,4 +14,4 @@ export const LIST_TRANSACTIONS_LIMIT = 20;
 export const SUPPORT_ALBY_CONNECTION_NAME = `ZapPlanner - Alby Hub`;
 export const SUPPORT_ALBY_LIGHTNING_ADDRESS = "hub@getalby.com";
 
-export const ALBY_PRO_PLAN = `pro_202411_usd_month`;
+export const ALBY_PRO_PLAN = `pro_202411_usd_year`;
