2111cd039fcf77b1dfc52d8147988d1dcfeea134
René Aaron <rene@twentyuno.net>
2025-03-28 13:01:46 +0100

    fix: cursor pointer

--- frontend/src/components/ui/dropdown-menu.tsx
+++ frontend/src/components/ui/dropdown-menu.tsx
@@ -84,6 +84,7 @@ const DropdownMenuItem = React.forwardRef<
     className={cn(
       "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       inset && "pl-8",
+      "cursor-pointer", // 👈️ Custom classes
       className
     )}
     {...props}
