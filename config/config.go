82c7c0cd182d389943bdd73baa038c55ef20d760
im-adithya <imadithyavardhan@gmail.com>
2025-04-23 23:37:47 +0530

    chore: use first instead of find

--- config/config.go
+++ config/config.go
@@ -152,7 +152,7 @@ func (cfg *config) Get(key string, encryptionKey string) (string, error) {
 
 func (cfg *config) get(key string, encryptionKey string, gormDB *gorm.DB) (string, error) {
 	var userConfig db.UserConfig
-	err := gormDB.Where(&db.UserConfig{Key: key}).Limit(1).Find(&userConfig).Error
+	err := gormDB.Where("key = ?", key).First(&userConfig).Error
 	if err != nil {
 		return "", fmt.Errorf("failed to get configuration value: %w", gormDB.Error)
 	}
