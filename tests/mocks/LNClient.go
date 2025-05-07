bc252ec4bb6455639e01c58179a2a0a922d54f07
Fmar <frnandu@gmail.com>
2025-05-07 23:18:14 +0200

    fix: missing WatchHoldInvoice  in the mocks

--- tests/mocks/LNClient.go
+++ tests/mocks/LNClient.go
@@ -2014,7 +2014,54 @@ func (_c *MockLNClient_UpdateLastWalletSyncRequest_Call) Return() *MockLNClient_
 }
 
 func (_c *MockLNClient_UpdateLastWalletSyncRequest_Call) RunAndReturn(run func()) *MockLNClient_UpdateLastWalletSyncRequest_Call {
-	_c.Run(run)
+_c.Run(run)
+return _c
+}
+
+// WatchHoldInvoice provides a mock function with given fields: ctx, paymentHash
+func (_m *MockLNClient) WatchHoldInvoice(ctx context.Context, paymentHash string) error {
+	ret := _m.Called(ctx, paymentHash)
+
+	if len(ret) == 0 {
+		panic("no return value specified for WatchHoldInvoice")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string) error); ok {
+		r0 = rf(ctx, paymentHash)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// MockLNClient_WatchHoldInvoice_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'WatchHoldInvoice'
+type MockLNClient_WatchHoldInvoice_Call struct {
+	*mock.Call
+}
+
+// WatchHoldInvoice is a helper method to define mock.On call
+//   - ctx context.Context
+//   - paymentHash string
+func (_e *MockLNClient_Expecter) WatchHoldInvoice(ctx interface{}, paymentHash interface{}) *MockLNClient_WatchHoldInvoice_Call {
+	return &MockLNClient_WatchHoldInvoice_Call{Call: _e.mock.On("WatchHoldInvoice", ctx, paymentHash)}
+}
+
+func (_c *MockLNClient_WatchHoldInvoice_Call) Run(run func(ctx context.Context, paymentHash string)) *MockLNClient_WatchHoldInvoice_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(string))
+	})
+	return _c
+}
+
+func (_c *MockLNClient_WatchHoldInvoice_Call) Return(_a0 error) *MockLNClient_WatchHoldInvoice_Call {
+	_c.Call.Return(_a0)
+	return _c
+}
+
+func (_c *MockLNClient_WatchHoldInvoice_Call) RunAndReturn(run func(context.Context, string) error) *MockLNClient_WatchHoldInvoice_Call {
+	_c.Call.Return(run)
 	return _c
 }
 
