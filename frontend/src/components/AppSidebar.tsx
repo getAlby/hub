96698bea7c912adf9d91248b18e290e5c950a314
im-adithya <imadithyavardhan@gmail.com>
2025-05-13 20:09:57 +0530

    chore: add defaults for name and ln address in sidebar

--- frontend/src/components/AppSidebar.tsx
+++ frontend/src/components/AppSidebar.tsx
@@ -181,19 +181,19 @@ export function AppSidebar() {
                   size="lg"
                   className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                 >
-                  {info?.albyAccountConnected && (
-                    <>
-                      <UserAvatar />
-                      <div className="grid flex-1 text-left text-sm leading-tight">
-                        <span className="truncate font-semibold">
-                          {albyMe?.name || albyMe?.email}
-                        </span>
-                        <div className="truncate text-xs">
-                          {albyMe?.lightning_address}
-                        </div>
-                      </div>
-                    </>
-                  )}
+                  <UserAvatar />
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">
+                      {info?.albyAccountConnected
+                        ? albyMe?.name || albyMe?.email
+                        : "Satoshi Nakamoto"}
+                    </span>
+                    <div className="truncate text-xs">
+                      {info?.albyAccountConnected
+                        ? albyMe?.lightning_address
+                        : "satoshin@gmx.com"}
+                    </div>
+                  </div>
                   <ChevronsUpDown className="ml-auto size-4" />
                 </SidebarMenuButton>
               </DropdownMenuTrigger>
