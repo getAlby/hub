a8ebae41dbfb626680b2c3658017ce9b49b77045
im-adithya <imadithyavardhan@gmail.com>
2025-05-13 20:01:22 +0530

    feat: add info page to settings

--- frontend/src/components/layouts/SettingsLayout.tsx
+++ frontend/src/components/layouts/SettingsLayout.tsx
@@ -98,6 +98,7 @@ export default function SettingsLayout() {
       <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-4 lg:space-y-0">
         <aside className="flex flex-col justify-between lg:w-1/5 max-h-screen">
           <nav className="flex flex-wrap lg:flex-col lg:space-y-1">
+            <MenuItem to="/settings/about">About</MenuItem>
             <MenuItem to="/settings">General</MenuItem>
             {info?.autoUnlockPasswordSupported && (
               <MenuItem to="/settings/auto-unlock">Auto Unlock</MenuItem>
---
+++ frontend/src/screens/settings/About.tsx
@@ -0,0 +1,54 @@
+import Loading from "src/components/Loading";
+import SettingsHeader from "src/components/SettingsHeader";
+import { useAlbyMe } from "src/hooks/useAlbyMe";
+
+import { useInfo } from "src/hooks/useInfo";
+
+export function About() {
+  const { data: info } = useInfo();
+  const { data: albyMe } = useAlbyMe();
+
+  if (!info || !albyMe) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <SettingsHeader title="About" description="Info about your Alby Hub" />
+      <div className="grid gap-4">
+        <div className="grid gap-2">
+          <p className="font-medium text-sm">Alby Hub Version</p>
+          <p className="text-muted-foreground text-sm slashed-zero">
+            {info.version || "dev"}
+          </p>
+        </div>
+        <div className="grid gap-2">
+          <p className="font-medium text-sm">Lightning Node Backend</p>
+          <p className="text-muted-foreground text-sm slashed-zero">
+            {info.backendType}
+          </p>
+        </div>
+        <div className="grid gap-2">
+          <p className="font-medium text-sm">Nostr Relay</p>
+          <p className="text-muted-foreground text-sm slashed-zero">
+            {info.relay}
+          </p>
+        </div>
+        {info.albyAccountConnected && albyMe && (
+          <div className="grid gap-2">
+            <p className="font-medium text-sm">Connected Alby Account</p>
+            <p className="text-muted-foreground text-sm slashed-zero">
+              {albyMe.email || albyMe.lightning_address}
+            </p>
+          </div>
+        )}
+        <div className="grid gap-2">
+          <p className="font-medium text-sm">Alby Account Plan</p>
+          <p className="text-muted-foreground text-sm slashed-zero">
+            {albyMe?.subscription.plan_code || "Free"}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
