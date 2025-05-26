08d33bc4a83bf414b08d410417b53b4f3a3e6c4b
Roland <33993199+rolznz@users.noreply.github.com>
2025-05-26 17:07:47 +0700

    chore: remove unused alby lsp (#1340)

--- lnclient/ldk/ldk.go
+++ lnclient/ldk/ldk.go
@@ -76,12 +76,10 @@ func NewLDKService(ctx context.Context, cfg config.Config, eventPublisher events
 
 	ldkConfig.TrustedPeers0conf = []string{
 		lsp.OlympusLSP().Pubkey,
-		lsp.AlbyPlebsLSP().Pubkey,
 		lsp.MegalithLSP().Pubkey,
 		"02b4552a7a85274e4da01a7c71ca57407181752e8568b31d51f13c111a2941dce3", // LNServer_Wave
 
 		// Mutinynet
-		lsp.AlbyMutinynetPlebsLSP().Pubkey,
 		lsp.OlympusMutinynetLSP().Pubkey,
 		lsp.MegalithMutinynetLSP().Pubkey,
 	}
