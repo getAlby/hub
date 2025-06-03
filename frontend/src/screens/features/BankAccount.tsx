bcb37035a4e217f38c1feb8f0a12d460d224c009
René Aaron <rene@twentyuno.net>
2025-06-03 10:32:54 +0200

    feat: success state

--- frontend/src/screens/features/BankAccount.tsx
+++ frontend/src/screens/features/BankAccount.tsx
@@ -1,9 +1,15 @@
-import { CreditCardIcon } from "lucide-react";
+import { CheckCircle, CreditCardIcon } from "lucide-react";
+import { useState } from "react";
 import AppHeader from "src/components/AppHeader";
 import { Button } from "src/components/ui/button";
+import { localStorageKeys } from "src/constants";
 import { request } from "src/utils/request";
 
 function BankAccount() {
+  const [activated, setActivated] = useState(
+    !!localStorage.getItem(localStorageKeys.interestVirtualBankAccount)
+  );
+
   async function activate() {
     await request(`/api/event`, {
       method: "POST",
@@ -14,6 +20,9 @@ function BankAccount() {
         event: "interest_virtual_bankaccount",
       }),
     });
+
+    localStorage.setItem(localStorageKeys.interestVirtualBankAccount, "true");
+    setActivated(true);
   }
 
   return (
@@ -24,17 +33,31 @@ function BankAccount() {
       />
       <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm p-8">
         <div className="flex flex-col items-center gap-1 text-center max-w-sm">
-          <CreditCardIcon className="w-10 h-10 text-muted-foreground" />
-          <h3 className="mt-4 text-lg font-semibold">
-            Activate Your Virtual Bank Account
-          </h3>
-          <p className="text-sm text-muted-foreground">
-            Fund your Hub with fiat, settle traditional payments, and move money
-            to Lightning in seconds. Virtual credit card coming soon!
-          </p>
-          <Button onClick={activate} className="mt-4">
-            Request Early Access
-          </Button>
+          {!activated ? (
+            <>
+              <CreditCardIcon className="w-10 h-10 text-muted-foreground" />
+              <h3 className="mt-4 text-lg font-semibold">
+                Activate Your Virtual Bank Account
+              </h3>
+              <p className="text-sm text-muted-foreground">
+                Fund your Hub with fiat, settle traditional payments, and move
+                money to Lightning in seconds. Virtual credit card coming soon!
+              </p>
+              <Button onClick={activate} className="mt-4">
+                Request Early Access
+              </Button>
+            </>
+          ) : (
+            <>
+              <CheckCircle className="w-10 h-10 text-muted-foreground" />
+              <h3 className="mt-4 text-lg font-semibold">
+                Thanks for your interest!
+              </h3>
+              <p className="text-sm text-muted-foreground">
+                We'll let you know as soon as this feature is available.
+              </p>
+            </>
+          )}
         </div>
       </div>
     </>
