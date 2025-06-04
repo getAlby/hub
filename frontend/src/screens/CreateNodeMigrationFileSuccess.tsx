6aedcb6bbdc1f5745cda5a1ca2460147ed33eb13
im-adithya <imadithyavardhan@gmail.com>
2025-06-04 19:01:43 +0530

    fix: add prop to not render sidebar trigger in header

--- frontend/src/screens/CreateNodeMigrationFileSuccess.tsx
+++ frontend/src/screens/CreateNodeMigrationFileSuccess.tsx
@@ -9,6 +9,7 @@ export function CreateNodeMigrationFileSuccess() {
         <AppHeader
           title="Alby Hub Migration File Saved"
           description="You're ready to move your node to another machine"
+          hideTrigger
         />
         <Card>
           <CardContent className="pt-6">
