9a8f02f37e666ceef795b998c638478bcab5d027
im-adithya <imadithyavardhan@gmail.com>
2025-06-11 14:09:15 +0530

    chore: use icons instead of badges

--- frontend/src/screens/wallet/swap/SwapOutStatus.tsx
+++ frontend/src/screens/wallet/swap/SwapOutStatus.tsx
@@ -1,9 +1,14 @@
-import { CircleCheckIcon, CopyIcon, ExternalLinkIcon } from "lucide-react";
+import {
+  CircleCheckIcon,
+  CircleEllipsisIcon,
+  CircleXIcon,
+  CopyIcon,
+  ExternalLinkIcon,
+} from "lucide-react";
 import { Link, useLocation } from "react-router-dom";
 import AppHeader from "src/components/AppHeader";
 import ExternalLink from "src/components/ExternalLink";
 import FormattedFiatAmount from "src/components/FormattedFiatAmount";
-import { Badge } from "src/components/ui/badge";
 import { Button } from "src/components/ui/button";
 import {
   Card,
@@ -15,7 +20,6 @@ import { useToast } from "src/components/ui/use-toast";
 import { useSyncWallet } from "src/hooks/useSyncWallet";
 import { useTransaction } from "src/hooks/useTransaction";
 import { copyToClipboard } from "src/lib/clipboard";
-import { cn } from "src/lib/utils";
 import { SwapOutResponse } from "src/types";
 
 export default function SwapOutStatus() {
@@ -53,22 +57,18 @@ export default function SwapOutStatus() {
                 ? "Successful"
                 : swapStatus === "failed"
                   ? "Failed"
-                  : "Initiated"}
+                  : "Pending"}
             </CardTitle>
           </CardHeader>
           <CardContent className="flex flex-col items-center gap-4">
             {swapStatus === "success" ? (
               <CircleCheckIcon className="w-32 h-32 mb-2" />
+            ) : swapStatus === "failed" ? (
+              <CircleXIcon className="w-32 h-32 mb-2" />
             ) : (
-              <Badge
-                className={cn(swapStatus === "pending" && "animate-pulse")}
-              >
-                {" "}
-                {lightningPayment?.state || "Loading..."}
-              </Badge>
+              <CircleEllipsisIcon className="animate-pulse w-32 h-32 mb-2" />
             )}
 
-            {/*  */}
             <div className="flex flex-col gap-2 items-center">
               <p className="text-xl font-bold slashed-zero">
                 {new Intl.NumberFormat().format(amount)} sats
