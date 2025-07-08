066539c5366616dc2e2f7a595a998f7096b5fb10
Roland <33993199+rolznz@users.noreply.github.com>
2025-07-08 18:32:03 +0700

    docs: change rebalancing fee copy

--- frontend/src/components/RebalanceChannelDialogContent.tsx
+++ frontend/src/components/RebalanceChannelDialogContent.tsx
@@ -136,7 +136,7 @@ export function RebalanceChannelDialogContent({
               }}
             />
             <p className="mt-2 text-xs text-muted-foreground">
-              Fee: ~0.2%
+              Fee: ~0.2% + routing fees
               {!!amount && (
                 <> ({Math.floor(parseInt(amount || "0") * 0.01)} sats)</>
               )}
