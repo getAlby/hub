9a74bd41c4a26447d7f41cf7d7affb64b8b24cf7
Roland Bewick <roland.bewick@gmail.com>
2025-07-15 13:12:36 +0700

    fix: remove event deletion from migration

--- db/migrations/202508041712_delete_non_cascade_deleted_records.go
+++ db/migrations/202508041712_delete_non_cascade_deleted_records.go
@@ -18,26 +18,11 @@ var _202508041712_delete_non_cascade_deleted_records = &gormigrate.Migration{
 		// - app_permissions -> apps
 		// - request_events -> apps
 		// - response_events -> request_events
+		// however we will only delete app permissions.
+		// excess request and response events will be removed in batches by a background task.
 
-		if err := db.Transaction(func(tx *gorm.DB) error {
-
-			// delete orphaned app permissions
-			if err := tx.Exec(`DELETE FROM app_permissions
+		if err := db.Exec(`DELETE FROM app_permissions
 WHERE app_id NOT IN (SELECT id FROM apps)`).Error; err != nil {
-				return err
-			}
-
-			// delete request and response events, later we will setup a task to delete excess events
-			if err := tx.Exec(`DELETE FROM request_events`).Error; err != nil {
-				return err
-			}
-
-			if err := tx.Exec(`DELETE FROM response_events`).Error; err != nil {
-				return err
-			}
-
-			return nil
-		}); err != nil {
 			return err
 		}
 
