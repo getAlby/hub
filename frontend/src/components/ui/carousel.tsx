60a91e43de89ef827f2a862e4bb6ed8549d3b700
René Aaron <rene@twentyuno.net>
2025-08-05 13:29:04 +0200

    fix: lint

--- frontend/src/components/ui/carousel.tsx
+++ frontend/src/components/ui/carousel.tsx
@@ -61,7 +61,9 @@ function Carousel({
   const [canScrollNext, setCanScrollNext] = React.useState(false);
 
   const onSelect = React.useCallback((api: CarouselApi) => {
-    if (!api) return;
+    if (!api) {
+      return;
+    }
     setCanScrollPrev(api.canScrollPrev());
     setCanScrollNext(api.canScrollNext());
   }, []);
@@ -88,12 +90,16 @@ function Carousel({
   );
 
   React.useEffect(() => {
-    if (!api || !setApi) return;
+    if (!api || !setApi) {
+      return;
+    }
     setApi(api);
   }, [api, setApi]);
 
   React.useEffect(() => {
-    if (!api) return;
+    if (!api) {
+      return;
+    }
     onSelect(api);
     api.on("reInit", onSelect);
     api.on("select", onSelect);
