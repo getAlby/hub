c4ee271addf65132eb33c2dd5e874384e6d1e10e
Roland Bewick <roland.bewick@gmail.com>
2025-09-30 16:25:58 +0200

    chore: minor copy

--- frontend/src/components/ExternalOnchainWalletRequiredAlert.tsx
+++ frontend/src/components/ExternalOnchainWalletRequiredAlert.tsx
@@ -31,10 +31,11 @@ export function ExternalOnchainWalletRequiredAlert({
       <AlertTriangleIcon className="h-4 w-4" />
       <AlertTitle>External Wallet Required</AlertTitle>
       <AlertDescription>
-        It's recommended to make this payment with an external on-chain wallet.
-        You do not have enough funds in your hub's on-chain wallet to make this
-        payment. To protect your anchor reserves, you need to set aside at least{" "}
-        {new Intl.NumberFormat().format(channels.length * 25000)} sats on-chain.
+        It's recommended to make this payment with an external on-chain wallet
+        as you do not have enough funds in your hub's on-chain wallet to make
+        this payment. To protect your anchor reserves, you need to set aside at
+        least {new Intl.NumberFormat().format(channels.length * 25000)} sats
+        on-chain.
       </AlertDescription>
     </Alert>
   );
