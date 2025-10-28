9d9c90f7bfef3c80410f47ee04b7edde8344b008
Roland Bewick <roland.bewick@gmail.com>
2025-10-28 23:44:17 +0700

    fix: tests

--- tests/create_mock_relay.go
+++ tests/create_mock_relay.go
@@ -7,16 +7,34 @@ import (
 	"github.com/nbd-wtf/go-nostr"
 )
 
-type mockRelay struct {
+type mockSimplePool struct {
 	PublishedEvents []*nostr.Event
 }
 
-func NewMockRelay() *mockRelay {
-	return &mockRelay{}
+func NewMockSimplePool() *mockSimplePool {
+	return &mockSimplePool{}
 }
 
-func (relay *mockRelay) Publish(ctx context.Context, event nostr.Event) error {
+func (relay *mockSimplePool) PublishMany(ctx context.Context, relayUrls []string, event nostr.Event) chan nostr.PublishResult {
 	logger.Logger.WithField("event", event).Info("Mock Publishing event")
 	relay.PublishedEvents = append(relay.PublishedEvents, &event)
+
+	channel := make(chan nostr.PublishResult)
+	go func() {
+		channel <- nostr.PublishResult{
+			RelayURL: "wss://fakerelay.com/v1",
+		}
+		close(channel)
+	}()
+	return channel
+}
+
+func (relay *mockSimplePool) QuerySingle(
+	ctx context.Context,
+	urls []string,
+	filter nostr.Filter,
+	opts ...nostr.SubscriptionOption,
+) *nostr.RelayEvent {
+	logger.Logger.Error("Mock pool QuerySingle is not supported yet")
 	return nil
 }
