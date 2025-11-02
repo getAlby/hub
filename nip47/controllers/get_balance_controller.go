8c4f1c8b2914f86a5a6d696e71bb91eb58bf538e
fierillo <fierillo@protonmail.com>
2025-11-02 16:49:20 -0300

    feat: limit balance displayed in NWC to available budget for enhanced privacy

--- nip47/controllers/get_balance_controller.go
+++ nip47/controllers/get_balance_controller.go
@@ -1,67 +1,82 @@
 package controllers
 
 import (
-	"context"
+    "context"
 
-	"github.com/getAlby/hub/db"
-	"github.com/getAlby/hub/db/queries"
-	"github.com/getAlby/hub/logger"
-	"github.com/getAlby/hub/nip47/models"
-	"github.com/nbd-wtf/go-nostr"
-	"github.com/sirupsen/logrus"
+    "github.com/getAlby/hub/db"
+    "github.com/getAlby/hub/db/queries"
+    "github.com/getAlby/hub/logger"
+    "github.com/getAlby/hub/nip47/models"
+    "github.com/nbd-wtf/go-nostr"
+    "github.com/sirupsen/logrus"
 )
 
 const (
-	MSAT_PER_SAT = 1000
+    MSAT_PER_SAT = 1000
 )
 
 type getBalanceResponse struct {
-	Balance int64 `json:"balance"`
-	// MaxAmount     int    `json:"max_amount"`
-	// BudgetRenewal string `json:"budget_renewal"`
+    Balance int64 `json:"balance"`
+    // MaxAmount     int    `json:"max_amount"`
+    // BudgetRenewal string `json:"budget_renewal"`
 }
 
 // TODO: remove checkPermission - can it be a middleware?
 func (controller *nip47Controller) HandleGetBalanceEvent(ctx context.Context, nip47Request *models.Request, requestEventId uint, app *db.App, publishResponse publishFunc) {
 
-	logger.Logger.WithFields(logrus.Fields{
-		"request_event_id": requestEventId,
-	}).Debug("Getting balance")
+    logger.Logger.WithFields(logrus.Fields{
+        "request_event_id": requestEventId,
+    }).Debug("Getting balance")
 
-	balance := int64(0)
-	if app.Isolated {
-		balance = queries.GetIsolatedBalance(controller.db, app.ID)
-	} else {
-		balances, err := controller.lnClient.GetBalances(ctx, true)
-		balance = balances.Lightning.TotalSpendable
-		if err != nil {
-			logger.Logger.WithFields(logrus.Fields{
-				"request_event_id": requestEventId,
-			}).WithError(err).Error("Failed to fetch balance")
-			publishResponse(&models.Response{
-				ResultType: nip47Request.Method,
-				Error:      mapNip47Error(err),
-			}, nostr.Tags{})
-			return
-		}
-	}
+    balance := int64(0)
+    if app.Isolated {
+        balance = queries.GetIsolatedBalance(controller.db, app.ID)
+    } else {
+        balances, err := controller.lnClient.GetBalances(ctx, true)
+        balance = balances.Lightning.TotalSpendable
+        if err != nil {
+            logger.Logger.WithFields(logrus.Fields{
+                "request_event_id": requestEventId,
+            }).WithError(err).Error("Failed to fetch balance")
+            publishResponse(&models.Response{
+                ResultType: nip47Request.Method,
+                Error:      mapNip47Error(err),
+            }, nostr.Tags{})
+            return
+        }
 
-	responsePayload := &getBalanceResponse{
-		Balance: balance,
-	}
+        // Check if there's a budget set for pay_invoice (which applies to spending)
+        var appPermission db.AppPermission
+        err = controller.db.Where("app_id = ? AND scope = ?", app.ID, models.PAY_INVOICE_METHOD).First(&appPermission).Error
+        if err == nil && appPermission.MaxAmountSat > 0 {
+            // Calculate available budget
+            totalBudgetMsat := uint64(appPermission.MaxAmountSat) * MSAT_PER_SAT
+            usedBudgetMsat := queries.GetBudgetUsageSat(controller.db, &appPermission) * MSAT_PER_SAT
+            availableBudgetMsat := totalBudgetMsat - usedBudgetMsat
+            // Show the minimum of actual balance and available budget
+            if availableBudgetMsat < uint64(balance) {
+                balance = int64(availableBudgetMsat)
+            }
+        }
+    }
 
-	// this is not part of the spec and does not seem to be used
-	/*appPermission := db.AppPermission{}
-	controller.db.Where("app_id = ? AND request_method = ?", app.ID, models.PAY_INVOICE_METHOD).First(&appPermission)
+    logger.Logger.WithFields(logrus.Fields{"balance": balance}).Info("Returning balance")
+    responsePayload := &getBalanceResponse{
+        Balance: balance,
+    }
 
-	maxAmount := appPermission.MaxAmount
-	if maxAmount > 0 {
-		responsePayload.MaxAmount = maxAmount * MSAT_PER_SAT
-		responsePayload.BudgetRenewal = appPermission.BudgetRenewal
-	}*/
+    // this is not part of the spec and does not seem to be used
+    /*appPermission := db.AppPermission{}
+    controller.db.Where("app_id = ? AND request_method = ?", app.ID, models.PAY_INVOICE_METHOD).First(&appPermission)
 
-	publishResponse(&models.Response{
-		ResultType: nip47Request.Method,
-		Result:     responsePayload,
-	}, nostr.Tags{})
+    maxAmount := appPermission.MaxAmount
+    if maxAmount > 0 {
+        responsePayload.MaxAmount = maxAmount * MSAT_PER_SAT
+        responsePayload.BudgetRenewal = appPermission.BudgetRenewal
+    }*/
+
+    publishResponse(&models.Response{
+        ResultType: nip47Request.Method,
+        Result:     responsePayload,
+    }, nostr.Tags{})
 }
