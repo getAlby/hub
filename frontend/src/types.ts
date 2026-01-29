d567ba62a53548e5c971cfcce14cc8fbb1f64018
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2026-01-30 00:40:53 +0100

    feat: add hold invoice support for CLN backend

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -447,6 +447,10 @@ export type SetupNodeInfo = Partial<{
   clnCaCert?: string;
   clnClientCert?: string;
   clnClientKey?: string;
+  clnAddressHold?: string;
+  clnCaCertHold?: string;
+  clnClientCertHold?: string;
+  clnClientKeyHold?: string;
 }>;
 
 export type LSPType = "LSPS1";
