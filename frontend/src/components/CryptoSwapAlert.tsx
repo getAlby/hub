a284359bc8eed0959986e2c3b6dd7e8dfb9101b2
im-adithya <imadithyavardhan@gmail.com>
2026-02-26 16:30:28 +0530

    feat: add crypto swap alert if sending fails

---
+++ frontend/src/components/CryptoSwapAlert.tsx
@@ -0,0 +1,32 @@
+import { CoinsIcon, ExternalLinkIcon } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "src/components/ui/alert";
+import { ExternalLinkButton } from "./ui/custom/external-link-button";
+
+type CryptoSwapAlertProps = {
+  className?: string;
+};
+export function CryptoSwapAlert({ className }: CryptoSwapAlertProps) {
+  return (
+    <Alert className={className}>
+      <AlertTitle className="flex items-center gap-2">
+        <CoinsIcon className="h-4 w-4" />
+        Looking to pay to other Cryptocurrency?
+      </AlertTitle>
+      <AlertDescription className="text-xs gap-2 mt-1">
+        <p>
+          If you are trying to pay a non-Bitcoin payment destination, use
+          FixedFloat to swap and complete the payment across 70+ supported
+          cryptocurrencies.
+        </p>
+        <ExternalLinkButton
+          to="https://ff.io/?ref=qnnjvywb&from=BTCLN"
+          variant="outline"
+          className="text-foreground"
+        >
+          Pay with FixedFloat
+          <ExternalLinkIcon className="size-4" />
+        </ExternalLinkButton>
+      </AlertDescription>
+    </Alert>
+  );
+}
