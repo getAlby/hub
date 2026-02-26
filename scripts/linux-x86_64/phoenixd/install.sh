9701c726acbee75d6ab7deff5ddc83277936c4f7
Sergey B. <sergey3bv@gmail.com>
2026-02-26 08:19:30 +0300

    Making shell scripts POSIX compliant (#2015)
    * feat: usage of rsync for backups
    
    * refactor: making scripts POSIX compliant

--- scripts/linux-x86_64/phoenixd/install.sh
+++ scripts/linux-x86_64/phoenixd/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ALBYHUB_URL="https://getalby.com/install/hub/server-linux-x86_64.tar.bz2"
 PHOENIX_VERSION="0.1.5"
@@ -10,43 +10,43 @@ echo "⚡️ Welcome to AlbyHub"
 echo "-----------------------------------------"
 echo "Installing AlbyHub with phoenixd"
 echo ""
-read -p "Absolute install directory path (default: $HOME/albyhub-phoenixd): " USER_INSTALL_DIR
+printf "Absolute install directory path (default: %s/albyhub-phoenixd): " "$HOME"
+read USER_INSTALL_DIR
 
 INSTALL_DIR="${USER_INSTALL_DIR:-$HOME/albyhub-phoenixd}"
 
 echo "Installing phoenixd $PHOENIX_VERSION into $INSTALL_DIR"
 
-mkdir -p $INSTALL_DIR
+mkdir -p "$INSTALL_DIR"
 mkdir -p "$INSTALL_DIR/phoenixd"
 
-cd $INSTALL_DIR
+cd "$INSTALL_DIR" || exit 1
 
-wget $PHOENIX_URL
-unzip -j phoenix-$PHOENIX_VERSION-linux-x64.zip -d phoenixd
+wget "$PHOENIX_URL"
+unzip -j "phoenix-$PHOENIX_VERSION-linux-x64.zip" -d phoenixd
 
 mkdir -p "$INSTALL_DIR/albyhub"
-wget $ALBYHUB_URL
-tar xvf server-linux-x86_64.tar.bz2 --directory=albyhub
-if [[ $? -ne 0 ]]; then
+wget "$ALBYHUB_URL"
+if ! tar xvf server-linux-x86_64.tar.bz2 --directory=albyhub; then
   echo "Failed to unpack Alby Hub. Potentially bzip2 is missing"
   echo "Install it with sudo apt-get install bzip2"
-  exit
+  exit 1
 fi
 
 rm server-linux-x86_64.tar.bz2
-rm phoenix-$PHOENIX_VERSION-linux-x64.zip
+rm "phoenix-$PHOENIX_VERSION-linux-x64.zip"
 
 ### Create start scripts
-tee $INSTALL_DIR/phoenixd/start.sh > /dev/null << EOF
-#!/bin/bash
+tee "$INSTALL_DIR/phoenixd/start.sh" > /dev/null << EOF
+#!/bin/sh
 
 echo "Starting phoenixd"
 echo "Make sure to backup your phoenixd data in $INSTALL_DIR/phoenixd/data"
 PHOENIX_DATADIR="$INSTALL_DIR/phoenixd/data" $INSTALL_DIR/phoenixd/phoenixd --agree-to-terms-of-service --http-bind-ip=0.0.0.0
 EOF
 
-tee $INSTALL_DIR/albyhub/start.sh > /dev/null << EOF
-#!/bin/bash
+tee "$INSTALL_DIR/albyhub/start.sh" > /dev/null << EOF
+#!/bin/sh
 
 echo "Starting Alby Hub"
 phoenix_config_file=$INSTALL_DIR/phoenixd/data/phoenix.conf
@@ -54,8 +54,8 @@ PHOENIXD_AUTHORIZATION=\$(awk -F'=' '/^http-password/{print \$2}' "\$phoenix_con
 WORK_DIR="$INSTALL_DIR/albyhub/data" LN_BACKEND_TYPE=PHOENIX PHOENIXD_ADDRESS="http://localhost:9740" PHOENIXD_AUTHORIZATION=\$PHOENIXD_AUTHORIZATION LDK_GOSSIP_SOURCE="" $INSTALL_DIR/albyhub/bin/albyhub
 EOF
 
-tee $INSTALL_DIR/start.sh > /dev/null << EOF
-#!/bin/bash
+tee "$INSTALL_DIR/start.sh" > /dev/null << EOF
+#!/bin/sh
 
 $INSTALL_DIR/phoenixd/start.sh &
 # wait a bit until phoenixd is started
@@ -65,22 +65,25 @@ $INSTALL_DIR/albyhub/start.sh &
 echo "Started..."
 EOF
 
-chmod +x $INSTALL_DIR/start.sh
-chmod +x $INSTALL_DIR/phoenixd/start.sh
-chmod +x $INSTALL_DIR/albyhub/start.sh
+chmod +x "$INSTALL_DIR/start.sh"
+chmod +x "$INSTALL_DIR/phoenixd/start.sh"
+chmod +x "$INSTALL_DIR/albyhub/start.sh"
 
 echo ""
 echo ""
 echo "Installation done."
 echo ""
 
-read -p "Do you want to setup a systemd service? (y/n): " -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-  echo "Run $INSTALL_DIR/start.sh to start phoenixd and Alby Hub"
-  echo "DONE"
-  exit
-fi
+printf "Do you want to setup a systemd service? (y/n): "
+read REPLY
+case "$REPLY" in
+  [Yy]*) ;;
+  *)
+    echo "Run $INSTALL_DIR/start.sh to start phoenixd and Alby Hub"
+    echo "DONE"
+    exit
+    ;;
+esac
 
 sudo tee /etc/systemd/system/albyhub.service > /dev/null << EOF
 [Unit]
