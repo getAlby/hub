805cd50c30065ebc6854e50741c78250e36bfb84
René Aaron <rene@twentyuno.net>
2026-03-26 10:13:33 +0100

    fix: remove custom text styles

--- frontend/src/screens/wallet/swap/index.tsx
+++ frontend/src/screens/wallet/swap/index.tsx
@@ -449,12 +449,7 @@ function SwapOutForm() {
               id="internal"
               className="shrink-0"
             />
-            <Label
-              htmlFor="internal"
-              className="text-primary font-medium cursor-pointer"
-            >
-              On-chain balance
-            </Label>
+            <Label htmlFor="internal">On-chain balance</Label>
           </div>
           <div className="flex items-start space-x-2">
             <RadioGroupItem
@@ -462,12 +457,7 @@ function SwapOutForm() {
               id="external"
               className="shrink-0"
             />
-            <Label
-              htmlFor="external"
-              className="text-primary font-medium cursor-pointer"
-            >
-              External on-chain wallet
-            </Label>
+            <Label htmlFor="external">External on-chain wallet</Label>
           </div>
         </RadioGroup>
       </div>
